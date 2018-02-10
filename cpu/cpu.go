e666a6d2f2798f6dfd5139e8f868c8d61fdd5c4d
Tabet Sami <salph.tabet@gmail.com>
2018-02-10 20:23:41 +0100

    added registers to the cpu struct and modified types to uint

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -1,9 +1,16 @@
 package cpu
 
 type CPU struct {
-	name   string
-	P      int16
-	cycles int
+	AB     uint16 // Accumulator register
+	DBR    uint8  // Data bank register
+	D      uint16 // The direct register
+	K      uint8  // The program bank register
+	PC     uint16 // The program counter
+	P      uint16 // Processor status register
+	S      uint16 // The stack pointer register
+	X      uint16 // The X index register
+	Y      uint16 // The Y index register
+	cycles int    // Number of cycles
 }
 
 type cpuOperation func()
