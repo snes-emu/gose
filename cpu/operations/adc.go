2b501fee3288987674fd40deb2c7c503cca95b91
Tabet Sami <salph.tabet@gmail.com>
2018-02-10 22:05:18 +0100

    added boilerpalte for adc func

--- cpu/operations/adc.go
+++ cpu/operations/adc.go
@@ -1,5 +1,20 @@
 package cpu
 
+// adc performs an add with carry operation the formula is: accumulator = accumulator + data + carry
+func (cpu *CPU) adc func() {
+	if cpu.getDFlag() == 0 {
+		// Decial mode off -> binary arithmetic used
+	} else {
+		// Decimal mode on -> BCD arithmetic used
+	}
+}
+
 opcodes[0x61] = (cpu *CPU) func() {
 	
+	w := 1
+	if cpu.getDLRegister() == 0 {
+		w := 0
+	}
+
+	cpu.cycles += 7 - cpu.getMFlag() + w
 }
