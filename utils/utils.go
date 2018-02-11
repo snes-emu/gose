8f8d992222894d8b3d12419b718f96b2de2d4872
Tabet Sami <salph.tabet@gmail.com>
2018-02-11 18:07:21 +0100

    Add(utils): added table conversion from bool to uint

---
+++ utils/utils.go
@@ -0,0 +1,13 @@
+package utils
+
+// BoolToUint16 provides a conversion from bool to uint16
+var BoolToUint16 = map[bool]uint16{
+	true:  1,
+	false: 0,
+}
+
+// BoolToUint8 provides a conversion from bool to uint8
+var BoolToUint8 = map[bool]uint8{
+	true:  1,
+	false: 0,
+}
