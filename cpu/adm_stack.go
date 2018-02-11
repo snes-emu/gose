5ac1713f7a1ce9c199bac4328aa68350ce265648
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-11 17:35:20 +0100

    cpu: add stack adm

---
+++ cpu/adm_stack.go
@@ -0,0 +1,18 @@
+package cpu
+
+// STACK,S addressing mode
+func (cpu CPU) admStackS(LL uint8) (uint8, uint8) {
+	laddress := uint32(cpu.getSRegister() + uint16(LL))
+	haddress := uint32(cpu.getSRegister() + uint16(LL) + 1)
+	return cpu.memory.GetByte(haddress), cpu.memory.GetByte(laddress)
+}
+
+// (STACK,S),Y addressing mode
+func (cpu CPU) admStackSY(LL uint8) (uint8, uint8) {
+	laddress := uint32(cpu.getSRegister() + uint16(LL))
+	haddress := uint32(cpu.getSRegister() + uint16(LL) + 1)
+	ll := cpu.memory.GetByte(laddress)
+	hh := cpu.memory.GetByte(haddress)
+	pointer := readUint32(cpu.getDBRRegister(), hh, ll) + uint32(cpu.getYRegister())
+	return cpu.memory.GetByte(pointer + 1), cpu.memory.GetByte(pointer)
+}
