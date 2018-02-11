c077a5eced77c5757a04f22f109f920bf4448853
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-11 19:48:04 +0100

    memory: add get byte with bank

--- memory/memory.go
+++ memory/memory.go
@@ -7,3 +7,7 @@ type Memory struct {
 func (memory Memory) GetByte(index uint32) uint8 {
 	return memory.bytes[index]
 }
+
+func (memory Memory) GetByteBank(K uint8, address uint16) uint8 {
+	return memory.bytes[uint32(K)<<16 + +uint32(address)]
+}
