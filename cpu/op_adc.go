7ebea1698cb402fd9ffc0aa0b0be773553e5220c
Tabet Sami <salph.tabet@gmail.com>
2018-02-11 19:20:40 +0100

    Feature(op_adc): added adc8 operator

--- cpu/op_adc.go
+++ cpu/op_adc.go
@@ -2,9 +2,8 @@ package cpu
 
 import "github.com/snes-emu/gose/utils"
 
-// adc performs an add with carry operation the formula is: accumulator = accumulator + data + carry
-
-func (cpu *CPU) adc16(data uint16) {
+// adc16 performs an add with carry 16bit operation the formula is: accumulator = accumulator + data + carry
+func (cpu *CPU) adc16(data uint16) uint16 {
 	var result uint16
 	if cpu.dFlag {
 		// Decimal mode on -> BCD arithmetic used
@@ -14,7 +13,7 @@ func (cpu *CPU) adc16(data uint16) {
 		// Decimal mode off -> binary arithmetic used
 		result = cpu.getCRegister() + data + utils.BoolToUint16[cpu.cFlag]
 		// Last bit value
-		cpu.nFlag = utils.GetBit16(result, 15) != 0
+		cpu.nFlag = result&0x8000 != 0
 		// Signed artihmetic overflow
 		cpu.vFlag = (data^result)&^(data^cpu.getCRegister())&0x8000 != 0
 		cpu.zFlag = result == 0
@@ -22,6 +21,30 @@ func (cpu *CPU) adc16(data uint16) {
 		cpu.cFlag = (result < cpu.getCRegister()) || (result == cpu.getCRegister() && cpu.cFlag)
 
 	}
+
+	return result
+}
+
+// adc8 performs an add with carry 8bit operation the formula is: accumulator = accumulator + data + carry
+func (cpu *CPU) adc8(data uint8) uint8 {
+	var result uint8
+	if cpu.dFlag {
+		// Decimal mode on -> BCD arithmetic used
+		panic("TODO, d flag in adc needs to be implemented")
+	} else {
+		// Decimal mode off -> binary arithmetic used
+		result = cpu.getARegister() + data + utils.BoolToUint8[cpu.cFlag]
+		// Last bit value
+		cpu.nFlag = result&0x80 != 0
+		// Signed artihmetic overflow
+		cpu.vFlag = (data^result)&^(data^cpu.getARegister())&0x80 != 0
+		cpu.zFlag = result == 0
+		// Unsigned carry
+		cpu.cFlag = (result < cpu.getARegister()) || (result == cpu.getARegister() && cpu.cFlag)
+
+	}
+
+	return result
 }
 
 func (cpu *CPU) op61() {
--- utils/utils.go
+++ utils/utils.go
@@ -11,13 +11,3 @@ var BoolToUint8 = map[bool]uint8{
 	true:  1,
 	false: 0,
 }
-
-// GetBit8  nth bit of a uint8
-func GetBit8(m, n uint8) uint8 {
-	return (m >> n) % 2
-}
-
-// GetBit16  nth bit of a uint8
-func GetBit16(m, n uint16) uint16 {
-	return (m >> n) % 2
-}
