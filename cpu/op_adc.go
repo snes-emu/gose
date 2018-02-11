01c6f1ede2c64e384fe4b94dc5045ccaf688a43c
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-11 21:17:31 +0100

    cpu: adc: all opcodes

--- cpu/op_adc.go
+++ cpu/op_adc.go
@@ -48,123 +48,114 @@ func (cpu *CPU) adc8(data uint8) uint8 {
 	return result
 }
 
-func (cpu *CPU) op61() {
-
-	dataHi, dataLo := cpu.admPDirectX()
+// sbc performs a substract with carry operation handling the 8/16 bit cases
+func (cpu *CPU) adc(dataHi, dataLo uint8) {
 	if cpu.mFlag {
 		cpu.setARegister(cpu.adc8(dataLo))
 	} else {
 		cpu.setCRegister(cpu.adc16(utils.ReadUint16(dataHi, dataLo)))
 	}
+}
 
+func (cpu *CPU) op61() {
+
+	dataHi, dataLo := cpu.admPDirectX()
+	cpu.adc(dataHi, dataLo)
 	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
 }
 
 func (cpu *CPU) op63() {
 
 	dataHi, dataLo := cpu.admStackS()
-
-	if cpu.mFlag {
-		cpu.setARegister(cpu.adc8(dataLo))
-	} else {
-		cpu.setCRegister(cpu.adc16(utils.ReadUint16(dataHi, dataLo)))
-	}
-
+	cpu.adc(dataHi, dataLo)
 	cpu.cycles += 5 - utils.BoolToUint16[cpu.mFlag]
 }
 
 func (cpu *CPU) op65() {
 
 	dataHi, dataLo := cpu.admDirect()
-	if cpu.mFlag {
-		cpu.setARegister(cpu.adc8(dataLo))
-	} else {
-		cpu.setCRegister(cpu.adc16(utils.ReadUint16(dataHi, dataLo)))
-	}
-
+	cpu.adc(dataHi, dataLo)
 	cpu.cycles += 4 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
 }
 
 func (cpu *CPU) op67() {
 
 	dataHi, dataLo := cpu.admBDirect()
-	if cpu.mFlag {
-		cpu.setARegister(cpu.adc8(dataLo))
-	} else {
-		cpu.setCRegister(cpu.adc16(utils.ReadUint16(dataHi, dataLo)))
-	}
+	cpu.adc(dataHi, dataLo)
 	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
 }
 
 func (cpu *CPU) op69() {
 
 	dataHi, dataLo := cpu.admImmediate()
-	if cpu.mFlag {
-		cpu.setARegister(cpu.adc8(dataLo))
-	} else {
-		cpu.setCRegister(cpu.adc16(utils.ReadUint16(dataHi, dataLo)))
-	}
-
+	cpu.adc(dataHi, dataLo)
 	cpu.cycles += 3 - utils.BoolToUint16[cpu.mFlag]
 }
 
 func (cpu *CPU) op6D() {
 
 	dataHi, dataLo := cpu.admAbsolute()
-	if cpu.mFlag {
-		cpu.setARegister(cpu.adc8(dataLo))
-	} else {
-		cpu.setCRegister(cpu.adc16(utils.ReadUint16(dataHi, dataLo)))
-	}
-
+	cpu.adc(dataHi, dataLo)
 	cpu.cycles += 5 - utils.BoolToUint16[cpu.mFlag]
 }
 
 func (cpu *CPU) op71() {
 
 	dataHi, dataLo := cpu.admPDirectY()
-	if cpu.mFlag {
-		cpu.setARegister(cpu.adc8(dataLo))
-	} else {
-		cpu.setCRegister(cpu.adc16(utils.ReadUint16(dataHi, dataLo)))
-	}
-
+	cpu.adc(dataHi, dataLo)
 	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0] - utils.BoolToUint16[cpu.xFlag]*utils.BoolToUint16[cpu.pFlag]
+}
+
+func (cpu *CPU) op72() {
+	dataHi, dataLo := cpu.admPDirect()
+
+	cpu.adc(dataHi, dataLo)
+
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
 }
 
 func (cpu *CPU) op73() {
 
 	dataHi, dataLo := cpu.admPStackSY()
-	if cpu.mFlag {
-		cpu.setARegister(cpu.adc8(dataLo))
-	} else {
-		cpu.setCRegister(cpu.adc16(utils.ReadUint16(dataHi, dataLo)))
-	}
-
+	cpu.adc(dataHi, dataLo)
 	cpu.cycles += 8 - utils.BoolToUint16[cpu.mFlag]
+}
+
+func (cpu *CPU) op75() {
+	dataHi, dataLo := cpu.admDirectX()
+
+	cpu.adc(dataHi, dataLo)
+
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
 }
 
 func (cpu *CPU) op77() {
 
 	dataHi, dataLo := cpu.admDirectX()
-	if cpu.mFlag {
-		cpu.setARegister(cpu.adc8(dataLo))
-	} else {
-		cpu.setCRegister(cpu.adc16(utils.ReadUint16(dataHi, dataLo)))
-	}
-
+	cpu.adc(dataHi, dataLo)
 	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) op79() {
+	dataHi, dataLo := cpu.admAbsoluteX()
+
+	cpu.adc(dataHi, dataLo)
+
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.xFlag] + utils.BoolToUint16[cpu.mFlag]*utils.BoolToUint16[cpu.pFlag]
 }
 
 func (cpu *CPU) op7D() {
 
 	dataHi, dataLo := cpu.admAbsoluteY()
-	if cpu.mFlag {
-		cpu.setARegister(cpu.adc8(dataLo))
-	} else {
-		cpu.setCRegister(cpu.adc16(utils.ReadUint16(dataHi, dataLo)))
-	}
-
+	cpu.adc(dataHi, dataLo)
 	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0] - utils.BoolToUint16[cpu.xFlag]*utils.BoolToUint16[cpu.pFlag]
 
+}
+
+func (cpu *CPU) op7F() {
+	dataHi, dataLo := cpu.admLongX()
+
+	cpu.adc(dataHi, dataLo)
+
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag]
 }
