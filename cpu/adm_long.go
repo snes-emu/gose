24c1ff5d08ae24dca9a5443372a7e04ae889c7a6
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-11 16:39:59 +0100

    cpu: add long adm descriptions

--- cpu/adm_long.go
+++ cpu/adm_long.go
@@ -1,14 +1,17 @@
 package cpu
 
+// LONG addressing mode to use with JMP instruction only
 func (cpu CPU) admLongJ(LL uint8, MM uint8, HH uint8) uint32 {
 	return readUint32(HH, MM, LL)
 }
 
+// LONG addressing mode
 func (cpu CPU) admLong(LL uint8, MM uint8, HH uint8) (uint8, uint8) {
 	address := readUint32(HH, MM, LL)
 	return cpu.memory.GetByte(address + 1), cpu.memory.GetByte(address)
 }
 
+// LONG,X addressing mode
 func (cpu CPU) admLongX(LL uint8, MM uint8, HH uint8) (uint8, uint8) {
 	address := readUint32(HH, MM, LL) + uint32(cpu.getXRegister())
 	return cpu.memory.GetByte(address + 1), cpu.memory.GetByte(address)
