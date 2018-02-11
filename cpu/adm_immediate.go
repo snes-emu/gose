73f3329ced9bfd92bb027cf53c764aaddf733e41
Tabet Sami <salph.tabet@gmail.com>
2018-02-11 20:44:02 +0100

    Refactor(adm_immediate): refactored immediate op

--- cpu/adm_immediate.go
+++ cpu/adm_immediate.go
@@ -1,12 +1,12 @@
 package cpu
 
-// IMMEDIATE addressing mode with 8-bit data
-func (cpu CPU) admImmediate8() uint8 {
-	return cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+1)
-}
+// IMMEDIATE addressing mode with 16-bit/8-bit data depending on the m flag
+func (cpu CPU) admImmediate() (uint8, uint8) {
 
-// IMMEDIATE addressing mode with 16-bit data
-func (cpu CPU) admImmediate16() (uint8, uint8) {
+	if cpu.mFlag {
+		return cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+1), 0x00
+	}
+
 	LL := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+1)
 	HH := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+2)
 	return HH, LL
