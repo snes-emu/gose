2eb5c250a1c1027b50b7b0381e43631740188474
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-12 23:07:43 +0100

    cpu: add bmi

--- cpu/op_bmi.go
+++ cpu/op_bmi.go
@@ -1,1 +1,15 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+func (cpu *CPU) op30() {
+	offset := cpu.admRelative8()
+	t := cpu.nFlag
+	cpu.cycles += 2 + utils.BoolToUint16[t] + utils.BoolToUint16[t]*utils.BoolToUint16[cpu.eFlag]*utils.BoolToUint16[cpu.pFlag]
+	if t {
+		cpu.PC += offset + 2
+	} else {
+		cpu.PC += 2
+	}
+
+}
