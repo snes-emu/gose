409774339d454011fff98b0735690297d5663c14
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-12 22:43:35 +0100

    cpu: adm relative refactor

--- cpu/adm_relative.go
+++ cpu/adm_relative.go
@@ -1,19 +1,19 @@
 package cpu
 
 // RELATIVE8 addressing mode
-func (cpu CPU) admRelative8() uint32 {
+func (cpu CPU) admRelative8() uint16 {
 	LL := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+1)
 	switch t := LL >= 0; t {
 	case true:
-		return uint32(cpu.getKRegister())<<16 + uint32(cpu.getPCRegister()+2+uint16(LL))
+		return uint16(LL)
 	default:
-		return uint32(cpu.getKRegister())<<16 + uint32(cpu.getPCRegister()-254+uint16(LL))
+		return uint16(LL) - 256
 	}
 }
 
 // RELATIVE16 addressing mode
-func (cpu CPU) admRelative16() uint32 {
+func (cpu CPU) admRelative16() uint16 {
 	LL := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+1)
 	HH := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+2)
-	return uint32(cpu.getKRegister())<<16 + uint32(cpu.getPCRegister()+3+uint16(LL)+uint16(HH)<<8)
+	return uint16(LL) + uint16(HH)<<8
 }
