6f4db2bf9b228b05cd57447b2ead80f6055787a3
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-12 22:50:43 +0100

    cpu: add bra

--- cpu/op_bra.go
+++ cpu/op_bra.go
@@ -1,1 +1,9 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+func (cpu *CPU) op80() {
+	offset := cpu.admRelative8()
+	cpu.cycles += 3 + utils.BoolToUint16[cpu.eFlag]*utils.BoolToUint16[cpu.pFlag]
+	cpu.PC += offset + 2
+}
