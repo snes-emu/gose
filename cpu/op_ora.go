0f17ae00d87f68b952c40484119b96ea12b0fe03
Tabet Sami <salph.tabet@gmail.com>
2018-02-12 15:30:10 +0100

    Feature(op_ora): added opcodes for ora operation

--- cpu/op_ora.go
+++ cpu/op_ora.go
@@ -22,3 +22,93 @@ func (cpu *CPU) ora(dataHi, dataLo uint8) {
 		cpu.setCRegister(cpu.ora16(utils.ReadUint16(dataHi, dataLo)))
 	}
 }
+
+func (cpu *CPU) op01() {
+	dataHi, dataLo := cpu.admPDirectX()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) op03() {
+	dataHi, dataLo := cpu.admStackS()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.mFlag]
+}
+
+func (cpu *CPU) op05() {
+	dataHi, dataLo := cpu.admDirect()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 4 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) op07() {
+	dataHi, dataLo := cpu.admBDirect()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) op09() {
+	dataHi, dataLo := cpu.admImmediate()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 3 - utils.BoolToUint16[cpu.mFlag]
+}
+
+func (cpu *CPU) op0D() {
+	dataHi, dataLo := cpu.admAbsolute()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.mFlag]
+}
+
+func (cpu *CPU) op0F() {
+	dataHi, dataLo := cpu.admLong()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag]
+}
+
+func (cpu *CPU) op11() {
+	dataHi, dataLo := cpu.admPDirectY()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0] - utils.BoolToUint16[cpu.xFlag] + utils.BoolToUint16[cpu.xFlag]*utils.BoolToUint16[cpu.pFlag]
+}
+
+func (cpu *CPU) op12() {
+	dataHi, dataLo := cpu.admPDirect()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) op13() {
+	dataHi, dataLo := cpu.admPStackSY()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 8 - utils.BoolToUint16[cpu.mFlag]
+}
+
+func (cpu *CPU) op15() {
+	dataHi, dataLo := cpu.admDirectX()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) op17() {
+	dataHi, dataLo := cpu.admBDirectY()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) op19() {
+	dataHi, dataLo := cpu.admAbsoluteY()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag] - utils.BoolToUint16[cpu.xFlag] + utils.BoolToUint16[cpu.xFlag]*utils.BoolToUint16[cpu.pFlag]
+}
+
+func (cpu *CPU) op1D() {
+	dataHi, dataLo := cpu.admAbsoluteX()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag] - utils.BoolToUint16[cpu.xFlag] + utils.BoolToUint16[cpu.xFlag]*utils.BoolToUint16[cpu.pFlag]
+}
+
+func (cpu *CPU) op1F() {
+	dataHi, dataLo := cpu.admLongX()
+	cpu.ora(dataHi, dataLo)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag]
+}
