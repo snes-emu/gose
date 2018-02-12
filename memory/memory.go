77eda4f88af86c388f09739c5f7c0c518af897a0
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-12 16:29:38 +0100

    memory: add SetByte method

--- memory/memory.go
+++ memory/memory.go
@@ -11,3 +11,7 @@ func (memory Memory) GetByte(index uint32) uint8 {
 func (memory Memory) GetByteBank(K uint8, address uint16) uint8 {
 	return memory.bytes[uint32(K)<<16 + +uint32(address)]
 }
+
+func (memory Memory) SetByte(value uint8, index uint32) {
+	memory.bytes[index] = value
+}
