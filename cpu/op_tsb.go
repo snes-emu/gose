034c58c3167e180010fb8f903816c43c1255a85e
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-12 21:52:43 +0100

    cpu: tsb: update descriptions

--- cpu/op_tsb.go
+++ cpu/op_tsb.go
@@ -4,20 +4,20 @@ import (
 	"github.com/snes-emu/gose/utils"
 )
 
-// adc16 performs an add with carry 16bit operation the formula is: accumulator = accumulator + data + carry
+// tsb16 test the bits of the data with the bits of the accumulator then set the bits of the data that are ones in the accumulator 16bit operation
 func (cpu *CPU) tsb16(data uint16) uint16 {
 	cpu.zFlag = cpu.getCRegister()&data == 0
 	return data | cpu.getCRegister()
 
 }
 
-// adc8 performs an add with carry 8bit operation the formula is: accumulator = accumulator + data + carry
+// tsb8 test the bits of the data with the bits of the accumulator then set the bits of the data that are ones in the accumulator 16bit operation
 func (cpu *CPU) tsb8(data uint8) uint8 {
 	cpu.zFlag = cpu.getARegister()&data == 0
 	return data | cpu.getARegister()
 }
 
-// sbc performs a substract with carry operation handling the 8/16 bit cases
+// tsb test the bits of the data with the bits of the accumulator then set the bits of the data that are ones in the accumulator handling the 8/16 case
 func (cpu *CPU) tsb(addressHi, addressLo uint32) {
 	if cpu.mFlag {
 		cpu.memory.SetByte(cpu.tsb8(cpu.memory.GetByte(addressLo)), addressLo)
