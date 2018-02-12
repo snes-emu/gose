557cfde97030da47d085558bc8efc6e2da9cf559
Tabet Sami <salph.tabet@gmail.com>
2018-02-12 09:49:01 +0100

    Feature(cmp.go): added all the opcodes for cmp operator

--- cpu/op_cmp.go
+++ cpu/op_cmp.go
@@ -36,3 +36,108 @@ func (cpu *CPU) cmp(dataHi, dataLo uint8) {
 		cpu.setCRegister(cpu.sbc16(utils.ReadUint16(dataHi, dataLo)))
 	}
 }
+
+func (cpu *CPU) opC1() {
+
+	dataHi, dataLo := cpu.admPDirectX()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) opC3() {
+
+	dataHi, dataLo := cpu.admStackS()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.mFlag]
+}
+
+func (cpu *CPU) opC5() {
+
+	dataHi, dataLo := cpu.admDirect()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 4 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) opC7() {
+
+	dataHi, dataLo := cpu.admBDirect()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) opC9() {
+
+	dataHi, dataLo := cpu.admImmediate()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 3 - utils.BoolToUint16[cpu.mFlag]
+}
+
+func (cpu *CPU) opCD() {
+
+	dataHi, dataLo := cpu.admAbsolute()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.mFlag]
+}
+
+func (cpu *CPU) opCF() {
+
+	dataHi, dataLo := cpu.admLong()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag]
+}
+
+func (cpu *CPU) opD1() {
+
+	dataHi, dataLo := cpu.admPDirectY()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0] + utils.BoolToUint16[cpu.xFlag]*(utils.BoolToUint16[cpu.pFlag]-1)
+}
+
+func (cpu *CPU) opD2() {
+
+	dataHi, dataLo := cpu.admPDirect()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) opD3() {
+
+	dataHi, dataLo := cpu.admPStackSY()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 8 - utils.BoolToUint16[cpu.mFlag]
+}
+
+func (cpu *CPU) opD5() {
+
+	dataHi, dataLo := cpu.admDirectX()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) opD7() {
+
+	dataHi, dataLo := cpu.admBDirectY()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) opD9() {
+
+	dataHi, dataLo := cpu.admAbsoluteY()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.xFlag]*(utils.BoolToUint16[cpu.pFlag]-1)
+}
+
+func (cpu *CPU) opDD() {
+
+	dataHi, dataLo := cpu.admAbsoluteX()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.xFlag]*(utils.BoolToUint16[cpu.pFlag]-1)
+}
+
+func (cpu *CPU) opDF() {
+
+	dataHi, dataLo := cpu.admLongX()
+	cpu.cmp(dataHi, dataLo)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag]
+}
