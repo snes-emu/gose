88b5beef6378b7f60c83d263a75c3d59d151a381
Tabet Sami <salph.tabet@gmail.com>
2018-02-12 09:57:19 +0100

    Fix(op_cmp): it should not change the accumulator but only the n z and c flags

--- cpu/op_cmp.go
+++ cpu/op_cmp.go
@@ -3,7 +3,7 @@ package cpu
 import "github.com/snes-emu/gose/utils"
 
 // cmp16 does a 16bit comparison the accumulator to the data
-func (cpu *CPU) cmp16(data uint16) uint16 {
+func (cpu *CPU) cmp16(data uint16) {
 	result := cpu.getCRegister() - data
 
 	// Last bit value
@@ -11,12 +11,10 @@ func (cpu *CPU) cmp16(data uint16) uint16 {
 	cpu.zFlag = result == 0
 	// Unsigned carry
 	cpu.cFlag = cpu.getCRegister() >= data
-
-	return result
 }
 
 // cmp8 does a 8bit comparison the accumulator to the data
-func (cpu *CPU) cmp8(data uint8) uint8 {
+func (cpu *CPU) cmp8(data uint8) {
 	result := cpu.getARegister() - data
 
 	// Last bit value
@@ -24,16 +22,14 @@ func (cpu *CPU) cmp8(data uint8) uint8 {
 	cpu.zFlag = result == 0
 	// Unsigned carry
 	cpu.cFlag = cpu.getARegister() >= data
-
-	return result
 }
 
 // cmp compare the accumulator to the data handling the 16bit/8bit distinction
 func (cpu *CPU) cmp(dataHi, dataLo uint8) {
 	if cpu.mFlag {
-		cpu.setARegister(cpu.sbc8(dataLo))
+		cpu.sbc8(dataLo)
 	} else {
-		cpu.setCRegister(cpu.sbc16(utils.ReadUint16(dataHi, dataLo)))
+		cpu.sbc16(utils.ReadUint16(dataHi, dataLo))
 	}
 }
 
