5e9aaa49e5dc0bf0b5ed44b41b7ca94fd2fb4ed6
Tabet Sami <salph.tabet@gmail.com>
2018-02-12 09:35:10 +0100

    Feature(op_cmp): added basic compare operation

--- cpu/op_cmp.go
+++ cpu/op_cmp.go
@@ -1,1 +1,38 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+// cmp16 does a 16bit comparison the accumulator to the data
+func (cpu *CPU) cmp16(data uint16) uint16 {
+	result := cpu.getCRegister() - data
+
+	// Last bit value
+	cpu.nFlag = result&0x8000 != 0
+	cpu.zFlag = result == 0
+	// Unsigned carry
+	cpu.cFlag = cpu.getCRegister() >= data
+
+	return result
+}
+
+// cmp8 does a 8bit comparison the accumulator to the data
+func (cpu *CPU) cmp8(data uint8) uint8 {
+	result := cpu.getARegister() - data
+
+	// Last bit value
+	cpu.nFlag = result&0x80 != 0
+	cpu.zFlag = result == 0
+	// Unsigned carry
+	cpu.cFlag = cpu.getARegister() >= data
+
+	return result
+}
+
+// cmp compare the accumulator to the data handling the 16bit/8bit distinction
+func (cpu *CPU) cmp(dataHi, dataLo uint8) {
+	if cpu.mFlag {
+		cpu.setARegister(cpu.sbc8(dataLo))
+	} else {
+		cpu.setCRegister(cpu.sbc16(utils.ReadUint16(dataHi, dataLo)))
+	}
+}
