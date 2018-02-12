9b0ff8b373d70d20dab5ae2db4e182e4a597cddf
Tabet Sami <salph.tabet@gmail.com>
2018-02-12 23:06:02 +0100

    Feature(op_lsr): added basic lsr operation

--- cpu/op_lsr.go
+++ cpu/op_lsr.go
@@ -1,1 +1,85 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+// lsr16acc performs a right shift on the 16 bit accumulator
+func (cpu *CPU) lsr16acc() {
+	result := cpu.getCRegister() >> 1
+
+	// Get the highbit before shifting
+	cpu.cFlag = cpu.getCRegister()&0x8000 != 0
+
+	// Last bit value
+	cpu.nFlag = result&0x8000 != 0
+	cpu.zFlag = result == 0
+
+	cpu.setCRegister(result)
+}
+
+// lsr8 performs a right shift on the lower 8 bit accumulator
+func (cpu *CPU) lsr8acc() {
+	result := cpu.getARegister() >> 1
+
+	// Get the highbit before shifting
+	cpu.cFlag = cpu.getARegister()&0x80 != 0
+
+	// Last bit value
+	cpu.nFlag = result&0x80 != 0
+	cpu.zFlag = result == 0
+
+	cpu.setARegister(result)
+}
+
+// lsr16data performs a right shift on the 16 bit data
+func (cpu *CPU) lsr16data(haddress, laddress uint32) {
+	dataHi, dataLo := cpu.memory.GetByte(haddress), cpu.memory.GetByte(laddress)
+
+	data := utils.ReadUint16(dataHi, dataLo)
+
+	result := data >> 1
+
+	// Get the highbit before shifting
+	cpu.cFlag = data&0x8000 != 0
+
+	// Last bit value
+	cpu.nFlag = result&0x8000 != 0
+	cpu.zFlag = result == 0
+
+	resultHi, resultLo := utils.WriteUint16(data)
+
+	cpu.memory.SetByte(resultHi, haddress)
+	cpu.memory.SetByte(resultLo, laddress)
+}
+
+// lsr8data performs a right shift on the 8 bit data
+func (cpu *CPU) lsr8data(addr uint32) {
+	data := cpu.memory.GetByte(addr)
+
+	result := data >> 1
+
+	// Get the highbit before shifting
+	cpu.cFlag = data&0x80 != 0
+
+	// Last bit value
+	cpu.nFlag = result&0x80 != 0
+	cpu.zFlag = result == 0
+
+	cpu.memory.SetByte(result, addr)
+}
+
+// lsr performs a right shift taking care of 16/8bits cases
+func (cpu *CPU) lsr(haddress, laddress uint32, isAcc bool) {
+	if cpu.mFlag {
+		if isAcc {
+			cpu.lsr8acc()
+		} else {
+			cpu.lsr8data(laddress)
+		}
+	} else {
+		if isAcc {
+			cpu.lsr16acc()
+		} else {
+			cpu.lsr16data(haddress, laddress)
+		}
+	}
+}
