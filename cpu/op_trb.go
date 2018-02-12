c7d3a6ef08c7443249fbe227dfc80e9688f06e3d
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-12 21:52:54 +0100

    cpu: add trb

--- cpu/op_trb.go
+++ cpu/op_trb.go
@@ -1,1 +1,30 @@
 package cpu
+
+import (
+	"github.com/snes-emu/gose/utils"
+)
+
+// trb16 test the bits of the data with the bits of the accumulator then reset the bits of the data that are ones in the accumulator 16bit operation
+func (cpu *CPU) trb16(data uint16) uint16 {
+	cpu.zFlag = cpu.getCRegister()&data == 0
+	return data &^ cpu.getCRegister()
+
+}
+
+// trb8 test the bits of the data with the bits of the accumulator then reset the bits of the data that are ones in the accumulator 16bit operation
+func (cpu *CPU) trb8(data uint8) uint8 {
+	cpu.zFlag = cpu.getARegister()&data == 0
+	return data &^ cpu.getARegister()
+}
+
+// trb test the bits of the data with the bits of the accumulator then reset the bits of the data that are ones in the accumulator handling the 8/16 case
+func (cpu *CPU) trb(addressHi, addressLo uint32) {
+	if cpu.mFlag {
+		cpu.memory.SetByte(cpu.trb8(cpu.memory.GetByte(addressLo)), addressLo)
+	} else {
+		result := cpu.trb16(utils.ReadUint16(cpu.memory.GetByte(addressHi), cpu.memory.GetByte(addressLo)))
+		resultHi, resultLo := utils.WriteUint16(result)
+		cpu.memory.SetByte(resultLo, addressLo)
+		cpu.memory.SetByte(resultHi, addressHi)
+	}
+}
