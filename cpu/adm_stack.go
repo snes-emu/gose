c8f34f3294bfc30db297c2ae78b1d47343a95405
Tabet Sami <salph.tabet@gmail.com>
2018-02-13 23:11:11 +0100

    Feature(adm_stack): added pointer addressing modes to the stack addressing

--- cpu/adm_stack.go
+++ cpu/adm_stack.go
@@ -4,19 +4,31 @@ import "github.com/snes-emu/gose/utils"
 
 // STACK,S addressing mode
 func (cpu CPU) admStackS() (uint8, uint8) {
+	haddress, laddress := cpu.admStackSP()
+	return cpu.memory.GetByte(haddress), cpu.memory.GetByte(laddress)
+}
+
+// STACK,S addressing mode pointer
+func (cpu CPU) admStackSP() (uint32, uint32) {
 	LL := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+1)
 	laddress := uint32(cpu.getSRegister() + uint16(LL))
 	haddress := uint32(cpu.getSRegister() + uint16(LL) + 1)
-	return cpu.memory.GetByte(haddress), cpu.memory.GetByte(laddress)
+	return haddress, laddress
 }
 
 // (STACK,S),Y addressing mode
 func (cpu CPU) admPStackSY() (uint8, uint8) {
+	haddr, laddr := cpu.admPStackSYP()
+	return cpu.memory.GetByte(haddr), cpu.memory.GetByte(laddr)
+}
+
+// (STACK,S),Y addressing mode
+func (cpu CPU) admPStackSYP() (uint32, uint32) {
 	LL := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+1)
 	laddress := uint32(cpu.getSRegister() + uint16(LL))
 	haddress := uint32(cpu.getSRegister() + uint16(LL) + 1)
 	ll := cpu.memory.GetByte(laddress)
 	hh := cpu.memory.GetByte(haddress)
 	pointer := utils.ReadUint32(cpu.getDBRRegister(), hh, ll) + uint32(cpu.getYRegister())
-	return cpu.memory.GetByte(pointer + 1), cpu.memory.GetByte(pointer)
+	return pointer + 1, pointer
 }
