497c55a22d39e5e6a9ce7f8c75fa54383ac766d3
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-13 15:58:11 +0100

    feature(memory): SetByteBank function

--- memory/memory.go
+++ memory/memory.go
@@ -15,3 +15,7 @@ func (memory Memory) GetByteBank(K uint8, address uint16) uint8 {
 func (memory Memory) SetByte(value uint8, index uint32) {
 	memory.bytes[index] = value
 }
+
+func (memory Memory) SetByteBank(value uint8, K uint8, address uint16) {
+	memory.bytes[uint32(K)<<16 + +uint32(address)] = value
+}
