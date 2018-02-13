a48f54a4107a6955ccdd7c55b94fe02f6c7e0b6e
Marc <marc.zundel@student.ecp.fr>
2018-02-13 16:26:29 +0100

    feat(Returns): implements RTL and RTS operations

--- cpu/op_rtl.go
+++ cpu/op_rtl.go
@@ -1,1 +1,13 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+func (cpu *CPU) op6B() uint32 {
+	dataLo := cpu.pullStack()
+	dataHi := cpu.pullStack()
+	counter := utils.ReadUint16(dataHi, dataLo) + 1
+	kvalue := cpu.pullStack()
+	dataHi, dataLo = utils.WriteUint16(counter)
+	result := utils.ReadUint32(kvalue, dataHi, dataLo)
+	return result
+}
--- cpu/op_rts.go
+++ cpu/op_rts.go
@@ -1,1 +1,13 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+func (cpu *CPU) op60() uint32 {
+	dataLo := cpu.pullStack()
+	dataHi := cpu.pullStack()
+	counter := utils.ReadUint16(dataHi, dataLo) + 1
+	kvalue := cpu.getKRegister()
+	dataHi, dataLo = utils.WriteUint16(counter)
+	result := utils.ReadUint32(kvalue, dataHi, dataLo)
+	return result
+}
