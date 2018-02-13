7d56478c99366aebada6a6c0ab9230c7708fbd51
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-13 20:59:34 +0100

    feature(op_cop): add COP opcode

--- cpu/op_cop.go
+++ cpu/op_cop.go
@@ -1,1 +1,39 @@
 package cpu
+
+import (
+	"github.com/snes-emu/gose/utils"
+)
+
+func (cpu *CPU) op02() {
+	P := utils.BoolToUint8[cpu.cFlag]*0x01 +
+		utils.BoolToUint8[cpu.zFlag]*0x02 +
+		utils.BoolToUint8[cpu.iFlag]*0x04 +
+		utils.BoolToUint8[cpu.dFlag]*0x08 +
+		utils.BoolToUint8[cpu.xFlag]*0x10 +
+		utils.BoolToUint8[cpu.mFlag]*0x20 +
+		utils.BoolToUint8[cpu.vFlag]*0x40 +
+		utils.BoolToUint8[cpu.nFlag]*0x80
+	addressHi, addressLo := utils.WriteUint16(cpu.getPCRegister() + 2)
+	if cpu.eFlag {
+		cpu.pushStack(addressHi)
+		cpu.pushStack(addressLo)
+		cpu.pushStack(P)
+		cpu.K = 0x00
+		addressLo := cpu.memory.GetByteBank(0x00, 0xFFF4)
+		addressHi := cpu.memory.GetByteBank(0x00, 0xFFF5)
+		cpu.PC = utils.ReadUint16(addressHi, addressLo)
+	} else {
+		cpu.pushStack(cpu.getKRegister())
+		cpu.pushStack(addressHi)
+		cpu.pushStack(addressLo)
+		cpu.pushStack(P)
+		cpu.K = 0x00
+		addressLo := cpu.memory.GetByteBank(0x00, 0xFFE4)
+		addressHi := cpu.memory.GetByteBank(0x00, 0xFFE5)
+		cpu.PC = utils.ReadUint16(addressHi, addressLo)
+	}
+	cpu.dFlag = false
+	cpu.iFlag = true
+	cpu.cycles += 8 - utils.BoolToUint16[cpu.eFlag]
+
+}
