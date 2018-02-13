8a655fee1bcab27327724751deae0f904dd922cc
Tabet Sami <salph.tabet@gmail.com>
2018-02-13 22:49:12 +0100

    Feature(op_ldy): added opcodes for ldy operation

--- cpu/op_ldy.go
+++ cpu/op_ldy.go
@@ -30,3 +30,38 @@ func (cpu *CPU) ldy(dataHi, dataLo uint8) {
 		cpu.ldy16(utils.ReadUint16(dataHi, dataLo))
 	}
 }
+
+func (cpu *CPU) opA0() {
+	dataHi, dataLo := cpu.admImmediateM()
+	cpu.ldy(dataHi, dataLo)
+	cpu.cycles += 3 - utils.BoolToUint16[cpu.xFlag]
+	cpu.PC += 3 - utils.BoolToUint16[cpu.xFlag]
+}
+
+func (cpu *CPU) opA4() {
+	dataHi, dataLo := cpu.admDirect()
+	cpu.ldy(dataHi, dataLo)
+	cpu.cycles += 4 - utils.BoolToUint16[cpu.xFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) opAC() {
+	dataHi, dataLo := cpu.admAbsolute()
+	cpu.ldy(dataHi, dataLo)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.xFlag]
+	cpu.PC += 3
+}
+
+func (cpu *CPU) opB4() {
+	dataHi, dataLo := cpu.admDirectX()
+	cpu.ldy(dataHi, dataLo)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.xFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) opBC() {
+	dataHi, dataLo := cpu.admAbsoluteX()
+	cpu.ldy(dataHi, dataLo)
+	cpu.cycles += 6 - 2*utils.BoolToUint16[cpu.xFlag] + utils.BoolToUint16[cpu.xFlag]*utils.BoolToUint16[cpu.pFlag]
+	cpu.PC += 3
+}
