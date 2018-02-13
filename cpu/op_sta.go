6b2c22be934ce1942919f318163d23636446491e
Tabet Sami <salph.tabet@gmail.com>
2018-02-13 23:15:57 +0100

    Feature(op_sta): added opcodes for sta operation

--- cpu/op_sta.go
+++ cpu/op_sta.go
@@ -25,3 +25,101 @@ func (cpu *CPU) sta(haddr, laddr uint32) {
 		cpu.sta16(haddr, laddr)
 	}
 }
+
+func (cpu *CPU) op81() {
+	haddr, laddr := cpu.admPDirectXP()
+	cpu.sta(haddr, laddr)
+	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) op83() {
+	haddr, laddr := cpu.admStackSP()
+	cpu.sta(haddr, laddr)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.mFlag]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) op85() {
+	haddr, laddr := cpu.admDirectP()
+	cpu.sta(haddr, laddr)
+	cpu.cycles += 4 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) op87() {
+	haddr, laddr := cpu.admBDirectP()
+	cpu.sta(haddr, laddr)
+	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) op8D() {
+	haddr, laddr := cpu.admAbsoluteP()
+	cpu.sta(haddr, laddr)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.mFlag]
+	cpu.PC += 3
+}
+
+func (cpu *CPU) op8F() {
+	haddr, laddr := cpu.admLongP()
+	cpu.sta(haddr, laddr)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag]
+	cpu.PC += 4
+}
+
+func (cpu *CPU) op91() {
+	haddr, laddr := cpu.admPDirectYP()
+	cpu.sta(haddr, laddr)
+	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) op92() {
+	haddr, laddr := cpu.admPDirectP()
+	cpu.sta(haddr, laddr)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) op93() {
+	haddr, laddr := cpu.admPStackSYP()
+	cpu.sta(haddr, laddr)
+	cpu.cycles += 8 - utils.BoolToUint16[cpu.mFlag]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) op95() {
+	haddr, laddr := cpu.admDirectXP()
+	cpu.sta(haddr, laddr)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) op97() {
+	haddr, laddr := cpu.admBDirectYP()
+	cpu.sta(haddr, laddr)
+	cpu.cycles += 7 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) op99() {
+	haddr, laddr := cpu.admAbsoluteYP()
+	cpu.sta(haddr, laddr)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag]
+	cpu.PC += 3
+}
+
+func (cpu *CPU) op9D() {
+	haddr, laddr := cpu.admAbsoluteXP()
+	cpu.sta(haddr, laddr)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag]
+	cpu.PC += 3
+}
+
+func (cpu *CPU) op9F() {
+	haddr, laddr := cpu.admLongXP()
+	cpu.sta(haddr, laddr)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag]
+	cpu.PC += 4
+}
