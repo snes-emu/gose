6c70e81b9b0d911ce173de73106a4d8009b91839
Tabet Sami <salph.tabet@gmail.com>
2018-02-13 22:09:16 +0100

    Feature(op_jmp): added opcodes for jmp operation

--- cpu/op_jmp.go
+++ cpu/op_jmp.go
@@ -1,16 +1,47 @@
 package cpu
 
-import "github.com/snes-emu/gose/utils"
-
 // jmp jumps to the address specified by the addressing mode
 func (cpu *CPU) jmp(addr uint16) {
 	cpu.PC = addr
 }
 
 // jmpLong jumps to the address specified by the long addressing
-func (cpu *CPU) jmpLong(addr uint32) {
-	hiaddr, miaddr, loaddr := utils.WriteUint32(addr)
+func (cpu *CPU) jmpLong(haddr uint8, laddr uint16) {
+	cpu.K = haddr
+	cpu.PC = laddr
+}
 
-	cpu.K = hiaddr
-	cpu.PC = utils.ReadUint16(miaddr, loaddr)
+func (cpu *CPU) op4C() {
+	addr := cpu.admAbsoluteJ()
+	cpu.jmp(addr)
+	cpu.cycles += 3
+	cpu.PC += 3
+}
+
+func (cpu *CPU) op5C() {
+	haddr, laddr := cpu.admLongJ()
+	cpu.jmpLong(haddr, laddr)
+	cpu.cycles += 4
+	cpu.PC += 4
+}
+
+func (cpu *CPU) op6C() {
+	addr := cpu.admPAbsoluteJ()
+	cpu.jmp(addr)
+	cpu.cycles += 5
+	cpu.PC += 3
+}
+
+func (cpu *CPU) op7C() {
+	addr := cpu.admPAbsoluteXJ()
+	cpu.jmp(addr)
+	cpu.cycles += 6
+	cpu.PC += 3
+}
+
+func (cpu *CPU) opDC() {
+	haddr, laddr := cpu.admBAbsoluteJ()
+	cpu.jmpLong(haddr, laddr)
+	cpu.cycles += 6
+	cpu.PC += 3
 }
