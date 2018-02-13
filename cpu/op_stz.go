50c3d0b536285f186087cedab42dab6fbdc960d1
Tabet Sami <salph.tabet@gmail.com>
2018-02-13 23:28:01 +0100

    Feature(op_stz): added opcodes for stz operation

--- cpu/op_stz.go
+++ cpu/op_stz.go
@@ -25,3 +25,31 @@ func (cpu *CPU) stz(haddr, laddr uint32) {
 		cpu.stz16(haddr, laddr)
 	}
 }
+
+func (cpu *CPU) op64() {
+	haddr, laddr := cpu.admDirectP()
+	cpu.stz(haddr, laddr)
+	cpu.cycles += 4 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) op74() {
+	haddr, laddr := cpu.admDirectXP()
+	cpu.stz(haddr, laddr)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.mFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) op9C() {
+	haddr, laddr := cpu.admAbsoluteP()
+	cpu.stz(haddr, laddr)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.mFlag]
+	cpu.PC += 3
+}
+
+func (cpu *CPU) op9E() {
+	haddr, laddr := cpu.admAbsoluteXP()
+	cpu.stz(haddr, laddr)
+	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag]
+	cpu.PC += 3
+}
