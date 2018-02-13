c186cf296d9eeb8f9107ee2a05598d5bd05529cf
Tabet Sami <salph.tabet@gmail.com>
2018-02-13 21:59:12 +0100

    Feature(op_jmp): added jmp basic operation

--- cpu/op_jmp.go
+++ cpu/op_jmp.go
@@ -1,1 +1,16 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+// jmp jumps to the address specified by the addressing mode
+func (cpu *CPU) jmp(addr uint16) {
+	cpu.PC = addr
+}
+
+// jmpLong jumps to the address specified by the long addressing
+func (cpu *CPU) jmpLong(addr uint32) {
+	hiaddr, miaddr, loaddr := utils.WriteUint32(addr)
+
+	cpu.K = hiaddr
+	cpu.PC = utils.ReadUint16(miaddr, loaddr)
+}
