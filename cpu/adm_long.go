31130ecc54a4e6d3f89e8c23741284fa3d0a47f2
Tabet Sami <salph.tabet@gmail.com>
2018-02-13 23:13:29 +0100

    Feature(adm_long): added pointer addressing mode to the long addressing mode

--- cpu/adm_long.go
+++ cpu/adm_long.go
@@ -12,18 +12,30 @@ func (cpu CPU) admLongJ() (uint8, uint16) {
 
 // LONG addressing mode
 func (cpu CPU) admLong() (uint8, uint8) {
+	haddr, laddr := cpu.admLongP()
+	return cpu.memory.GetByte(haddr), cpu.memory.GetByte(laddr)
+}
+
+// LONG addressing mode pointer
+func (cpu CPU) admLongP() (uint32, uint32) {
 	LL := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+1)
 	MM := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+2)
 	HH := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+3)
 	address := utils.ReadUint32(HH, MM, LL)
-	return cpu.memory.GetByte(address + 1), cpu.memory.GetByte(address)
+	return address + 1, address
 }
 
 // LONG,X addressing mode
 func (cpu CPU) admLongX() (uint8, uint8) {
+	haddr, laddr := cpu.admLongXP()
+	return cpu.memory.GetByte(haddr), cpu.memory.GetByte(laddr)
+}
+
+// LONG,X addressing mode pointer
+func (cpu CPU) admLongXP() (uint32, uint32) {
 	LL := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+1)
 	MM := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+2)
 	HH := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+3)
 	address := utils.ReadUint32(HH, MM, LL) + uint32(cpu.getXRegister())
-	return cpu.memory.GetByte(address + 1), cpu.memory.GetByte(address)
+	return address + 1, address
 }
