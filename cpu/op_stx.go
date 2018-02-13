b5318056f2694ceadc2068aa8c95dbbbbea067f1
Tabet Sami <salph.tabet@gmail.com>
2018-02-13 23:22:41 +0100

    Feature(op_stx): added opcodes for stx operation

--- cpu/op_stx.go
+++ cpu/op_stx.go
@@ -25,3 +25,24 @@ func (cpu *CPU) stx(haddr, laddr uint32) {
 		cpu.stx16(haddr, laddr)
 	}
 }
+
+func (cpu *CPU) op86() {
+	haddr, laddr := cpu.admDirectP()
+	cpu.stx(haddr, laddr)
+	cpu.cycles += 4 - utils.BoolToUint16[cpu.xFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) op8E() {
+	haddr, laddr := cpu.admAbsoluteP()
+	cpu.stx(haddr, laddr)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.xFlag]
+	cpu.PC += 3
+}
+
+func (cpu *CPU) op96() {
+	haddr, laddr := cpu.admDirectYP()
+	cpu.stx(haddr, laddr)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.xFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
