a5f4b8f08911095c722ccc072fa1a620209ec0f1
Tabet Sami <salph.tabet@gmail.com>
2018-02-13 22:14:08 +0100

    Feature(op_jsl): added jsl basic operation

--- cpu/op_jsl.go
+++ cpu/op_jsl.go
@@ -1,1 +1,14 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+// jsl jumps to a subroutine long
+func (cpu *CPU) jsl(addr uint16) {
+	cpu.pushStack(cpu.getKRegister())
+	haddr, laddr := utils.WriteUint16(cpu.getPCRegister() + 3)
+
+	cpu.pushStack(haddr)
+	cpu.pushStack(laddr)
+
+	cpu.PC = addr
+}
