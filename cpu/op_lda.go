5f697c3f89f402285d5c7550a8893bb9017dd6a5
Tabet Sami <salph.tabet@gmail.com>
2018-02-13 22:39:13 +0100

    Feature(op_lda): added lda basic operation

--- cpu/op_lda.go
+++ cpu/op_lda.go
@@ -1,1 +1,32 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+// lda16 load data into the lower bits of the accumulator
+func (cpu *CPU) lda16(data uint16) {
+
+	// Last bit value
+	cpu.nFlag = data&0x8000 != 0
+	cpu.zFlag = data != 0
+
+	cpu.setCRegister(data)
+}
+
+// lda8 load data into the lower bits of the accumulator
+func (cpu *CPU) lda8(data uint8) {
+
+	// Last bit value
+	cpu.nFlag = data&0x80 != 0
+	cpu.zFlag = data != 0
+
+	cpu.setARegister(data)
+}
+
+// lda load data into the accumulator
+func (cpu *CPU) lda(dataHi, dataLo uint8) {
+	if cpu.mFlag {
+		cpu.lda8(dataLo)
+	} else {
+		cpu.lda16(utils.ReadUint16(dataHi, dataLo))
+	}
+}
