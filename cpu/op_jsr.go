87e06263cf9c1f2b7058d5d83acd49e113decf68
Tabet Sami <salph.tabet@gmail.com>
2018-02-13 22:19:24 +0100

    Feature(op_jsr): added basic operation for jsr

--- cpu/op_jsr.go
+++ cpu/op_jsr.go
@@ -1,1 +1,13 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+// jsr jumps to a subroutine
+func (cpu *CPU) jsr(addr uint16) {
+	haddr, laddr := utils.WriteUint16(cpu.getPCRegister() + 2)
+
+	cpu.pushStack(haddr)
+	cpu.pushStack(laddr)
+
+	cpu.jmp(addr)
+}
