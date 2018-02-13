b0b95c44a2b565a2af61070dcd8aaa349b58f2c8
Tabet Sami <salph.tabet@gmail.com>
2018-02-13 23:26:00 +0100

    Feature(op_sty): added opcodes for sty operation

--- cpu/op_sty.go
+++ cpu/op_sty.go
@@ -25,3 +25,24 @@ func (cpu *CPU) sty(haddr, laddr uint32) {
 		cpu.sty16(haddr, laddr)
 	}
 }
+
+func (cpu *CPU) op84() {
+	haddr, laddr := cpu.admDirectP()
+	cpu.sty(haddr, laddr)
+	cpu.cycles += 4 - utils.BoolToUint16[cpu.xFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
+
+func (cpu *CPU) op8C() {
+	haddr, laddr := cpu.admAbsoluteP()
+	cpu.sty(haddr, laddr)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.xFlag]
+	cpu.PC += 3
+}
+
+func (cpu *CPU) op94() {
+	haddr, laddr := cpu.admDirectXP()
+	cpu.sty(haddr, laddr)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.xFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+	cpu.PC += 2
+}
