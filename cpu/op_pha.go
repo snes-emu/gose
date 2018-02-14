38672ef1bd344d3a2156d38ad57f8b8426a58c1b
Tabet Sami <salph.tabet@gmail.com>
2018-02-14 13:35:46 +0100

    Feature(op_pha): added basic operation for pha

--- cpu/op_pha.go
+++ cpu/op_pha.go
@@ -1,1 +1,26 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+// pha16 push the accumulator onto the stack
+func (cpu *CPU) pha16() {
+	dataHi, dataLo := utils.WriteUint16(cpu.getCRegister())
+
+	cpu.pushStack(dataHi)
+	cpu.pushStack(dataLo)
+}
+
+// pha8 push the lower bit of the accumulator onto the stack
+func (cpu *CPU) pha8() {
+	data := cpu.getARegister()
+
+	cpu.pushStack(data)
+}
+
+func (cpu *CPU) pha() {
+	if cpu.mFlag {
+		cpu.pha8()
+	} else {
+		cpu.pha16()
+	}
+}
