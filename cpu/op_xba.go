73572e30ceab88c88a0f7458cf64434436133655
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-14 14:47:20 +0100

    feature(op_xba): add XBA instruction

--- cpu/op_xba.go
+++ cpu/op_xba.go
@@ -1,1 +1,11 @@
 package cpu
+
+func (cpu *CPU) opEB() {
+	temp := cpu.getBRegister()
+	cpu.setBRegister(cpu.getARegister())
+	cpu.setARegister(temp)
+	cpu.nFlag = temp&0x80 != 0
+	cpu.zFlag = temp == 0
+	cpu.cycles += 3
+	cpu.PC++
+}
