1dd84e38688675eb29669709a5aa58a998fe8fee
Marc <marc.zundel@student.ecp.fr>
2018-02-14 14:03:17 +0100

    feat(push): adds pushing of data to stack (PEA, PEI, PER)

--- cpu/op_pea.go
+++ cpu/op_pea.go
@@ -1,1 +1,13 @@
 package cpu
+
+import (
+	"github.com/snes-emu/gose/utils"
+)
+
+//opF4 pushes the next 16bit value into the stack
+func (cpu *CPU) opF4(data uint16) {
+	dataHi, dataLo := utils.WriteUint16(data)
+	cpu.pushStack(dataHi)
+	cpu.pushStack(dataLo)
+	cpu.cycles += 5
+}
--- cpu/op_per.go
+++ cpu/op_per.go
@@ -1,1 +1,13 @@
 package cpu
+
+import (
+	"github.com/snes-emu/gose/utils"
+)
+
+//op62 pushes 16bit data into the stack, called thanks to the next 8bit value
+func (cpu *CPU) op62(data uint16) {
+	dataHi, dataLo := utils.WriteUint16(data)
+	cpu.pushStack(dataHi)
+	cpu.pushStack(dataLo)
+	cpu.cycles += 6
+}
