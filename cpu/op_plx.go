184d8119b189fbda196420525096871963ca5d00
Tabet Sami <salph.tabet@gmail.com>
2018-02-14 13:45:09 +0100

    Feature(op_plx): added basic operation for plx

--- cpu/op_plx.go
+++ cpu/op_plx.go
@@ -1,1 +1,34 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+// plx16 pull the X register from the stack
+func (cpu *CPU) plx16() {
+	dataLo := cpu.pullStack()
+	dataHi := cpu.pullStack()
+
+	result := utils.ReadUint16(dataHi, dataLo)
+
+	cpu.nFlag = result&0x8000 != 0
+	cpu.zFlag = result != 0
+
+	cpu.setXRegister(result)
+}
+
+// plx8 pull the lower bits of the X register from the stack
+func (cpu *CPU) plx8() {
+	result := cpu.pullStack()
+
+	cpu.nFlag = result&0x80 != 0
+	cpu.zFlag = result != 0
+
+	cpu.setXLRegister(result)
+}
+
+func (cpu *CPU) plx() {
+	if cpu.xFlag {
+		cpu.plx8()
+	} else {
+		cpu.plx16()
+	}
+}
