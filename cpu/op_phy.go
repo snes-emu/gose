ce29e7c8c48ffc5ea193edc7b4726131f0a3b7a0
Tabet Sami <salph.tabet@gmail.com>
2018-02-14 13:39:13 +0100

    Feature(op_phy): added basic operation for phy

--- cpu/op_phy.go
+++ cpu/op_phy.go
@@ -1,1 +1,26 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+// phy16 push the Y register onto the stack
+func (cpu *CPU) phy16() {
+	dataHi, dataLo := utils.WriteUint16(cpu.getYRegister())
+
+	cpu.pushStack(dataHi)
+	cpu.pushStack(dataLo)
+}
+
+// phy8 push the lower bit of the Y register onto the stack
+func (cpu *CPU) phy8() {
+	data := cpu.getYLRegister()
+
+	cpu.pushStack(data)
+}
+
+func (cpu *CPU) phy() {
+	if cpu.xFlag {
+		cpu.phy8()
+	} else {
+		cpu.phy16()
+	}
+}
