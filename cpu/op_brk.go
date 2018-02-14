d993ac830d5ab118d14a95429e4d3cce3e35c4e5
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-14 14:27:42 +0100

    fix(op_brk): use php function

--- cpu/op_brk.go
+++ cpu/op_brk.go
@@ -5,20 +5,12 @@ import (
 )
 
 func (cpu *CPU) op00() {
-	P := utils.BoolToUint8[cpu.cFlag]*0x01 +
-		utils.BoolToUint8[cpu.zFlag]*0x02 +
-		utils.BoolToUint8[cpu.iFlag]*0x04 +
-		utils.BoolToUint8[cpu.dFlag]*0x08 +
-		utils.BoolToUint8[cpu.mFlag]*0x20 +
-		utils.BoolToUint8[cpu.vFlag]*0x40 +
-		utils.BoolToUint8[cpu.nFlag]*0x80
 	addressHi, addressLo := utils.WriteUint16(cpu.getPCRegister() + 2)
 	if cpu.eFlag {
 		cpu.pushStack(addressHi)
 		cpu.pushStack(addressLo)
 		cpu.bFlag = true
-		P += utils.BoolToUint8[cpu.bFlag] * 0x10
-		cpu.pushStack(P)
+		cpu.php()
 		cpu.K = 0x00
 		addressLo := cpu.memory.GetByteBank(0x00, 0xFFFE)
 		addressHi := cpu.memory.GetByteBank(0x00, 0xFFFF)
@@ -27,8 +19,7 @@ func (cpu *CPU) op00() {
 		cpu.pushStack(cpu.getKRegister())
 		cpu.pushStack(addressHi)
 		cpu.pushStack(addressLo)
-		P += utils.BoolToUint8[cpu.xFlag] * 0x10
-		cpu.pushStack(P)
+		cpu.php()
 		cpu.K = 0x00
 		addressLo := cpu.memory.GetByteBank(0x00, 0xFFE6)
 		addressHi := cpu.memory.GetByteBank(0x00, 0xFFE7)
