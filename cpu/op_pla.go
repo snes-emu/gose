5214dc6058311b09b1b110e498742303f6118586
Tabet Sami <salph.tabet@gmail.com>
2018-02-14 13:43:25 +0100

    Feature(op_pla): adde basic operation for pla

--- cpu/op_pla.go
+++ cpu/op_pla.go
@@ -1,1 +1,34 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+// pla16 pull the accumulator from the stack
+func (cpu *CPU) pla16() {
+	dataLo := cpu.pullStack()
+	dataHi := cpu.pullStack()
+
+	result := utils.ReadUint16(dataHi, dataLo)
+
+	cpu.nFlag = result&0x8000 != 0
+	cpu.zFlag = result != 0
+
+	cpu.setCRegister(result)
+}
+
+// pla8 pull the lower bits of the accumulator from the stack
+func (cpu *CPU) pla8() {
+	result := cpu.pullStack()
+
+	cpu.nFlag = result&0x80 != 0
+	cpu.zFlag = result != 0
+
+	cpu.setARegister(result)
+}
+
+func (cpu *CPU) pla() {
+	if cpu.mFlag {
+		cpu.pla8()
+	} else {
+		cpu.pla16()
+	}
+}
