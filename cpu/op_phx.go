2dd4f14b947eb3437f5675a304c655a95644fad5
Tabet Sami <salph.tabet@gmail.com>
2018-02-14 13:37:31 +0100

    Feature(op_phx): added basic operation for phx

--- cpu/op_phx.go
+++ cpu/op_phx.go
@@ -1,1 +1,26 @@
 package cpu
+
+import "github.com/snes-emu/gose/utils"
+
+// phx16 push the X register onto the stack
+func (cpu *CPU) phx16() {
+	dataHi, dataLo := utils.WriteUint16(cpu.getXRegister())
+
+	cpu.pushStack(dataHi)
+	cpu.pushStack(dataLo)
+}
+
+// phx8 push the lower bit of the X register onto the stack
+func (cpu *CPU) phx8() {
+	data := cpu.getXLRegister()
+
+	cpu.pushStack(data)
+}
+
+func (cpu *CPU) phx() {
+	if cpu.xFlag {
+		cpu.phx8()
+	} else {
+		cpu.phx16()
+	}
+}
