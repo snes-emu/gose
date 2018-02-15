eb9899e0b4183d29c0925bbd9d986372f31ed3ec
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-15 10:59:20 +0100

    feature(op_mvp): add MVP opcode

--- cpu/op_mvp.go
+++ cpu/op_mvp.go
@@ -1,1 +1,19 @@
 package cpu
+
+func (cpu *CPU) op44() {
+	SBank, SAddress, DBank, DAddress := cpu.admSourceDestination()
+	cpu.memory.SetByteBank(cpu.memory.GetByteBank(SBank, SAddress), DBank, DAddress)
+	cpu.DBR = DBank
+	cpu.C--
+	if cpu.xFlag {
+		cpu.setXLRegister(cpu.getXLRegister() - 1)
+		cpu.setYLRegister(cpu.getYLRegister() - 1)
+	} else {
+		cpu.X--
+		cpu.Y--
+	}
+	cpu.cycles += 7
+	if cpu.getCRegister() == 0xFFFF {
+		cpu.PC += 3
+	}
+}
