279571f5e11181737fcd79d851a2f3523cfca67c
Tabet Sami <salph.tabet@gmail.com>
2018-02-16 09:38:03 +0100

    Feature(memory): added a constructor

--- memory/memory.go
+++ memory/memory.go
@@ -4,6 +4,12 @@ type Memory struct {
 	bytes []uint8
 }
 
+func New() *Memory {
+	return &Memory{
+		make([]byte, 65535),
+	}
+}
+
 func (memory Memory) GetByte(index uint32) uint8 {
 	return memory.bytes[index]
 }
