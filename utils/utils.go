2b6fb30dd59ffdf57cbfa20a791106ed6481f669
Tabet Sami <salph.tabet@gmail.com>
2018-02-16 13:11:52 +0100

    Refactor(*): renamed ReadUint to JoinUint and WriteUint to SplitUint for more clarity

--- utils/utils.go
+++ utils/utils.go
@@ -17,21 +17,21 @@ var BoolToUint8 = map[bool]uint8{
 	false: 0,
 }
 
-func ReadUint32(HH uint8, MM uint8, LL uint8) uint32 {
+func JoinUint32(HH uint8, MM uint8, LL uint8) uint32 {
 	var ret uint32
 	buf := bytes.NewBuffer([]byte{LL, MM, HH})
 	binary.Read(buf, binary.LittleEndian, &ret)
 	return ret
 }
 
-func ReadUint16(HH uint8, LL uint8) uint16 {
+func JoinUint16(HH uint8, LL uint8) uint16 {
 	var ret uint16
 	buf := bytes.NewBuffer([]byte{LL, HH})
 	binary.Read(buf, binary.LittleEndian, &ret)
 	return ret
 }
 
-func WriteUint16(MM uint16) (uint8, uint8) {
+func SplitUint16(MM uint16) (uint8, uint8) {
 
 	buf := new(bytes.Buffer)
 	binary.Write(buf, binary.LittleEndian, &MM)
@@ -39,7 +39,7 @@ func WriteUint16(MM uint16) (uint8, uint8) {
 	return ret[1], ret[0]
 }
 
-func WriteUint32(MM uint32) (uint8, uint8, uint8) {
+func SplitUint32(MM uint32) (uint8, uint8, uint8) {
 
 	buf := new(bytes.Buffer)
 	binary.Write(buf, binary.LittleEndian, &MM)
