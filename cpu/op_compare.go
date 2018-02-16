2e1571e326acb5f15274cbb0e53e138ea3e7b549
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-16 15:24:58 +0100

    refactor(op_compare): merge cmp,cpx,cpy instruction files

--- cpu/op_compare.go
+++ cpu/op_compare.go
@@ -137,3 +137,101 @@ func (cpu *CPU) opDF() {
 	cpu.cmp(dataHi, dataLo)
 	cpu.cycles += 6 - utils.BoolToUint16[cpu.mFlag]
 }
+
+// cpx16 does a 16bit comparison of the X register with the data
+func (cpu *CPU) cpx16(data uint16) {
+	result := cpu.getXRegister() - data
+
+	// Last bit value
+	cpu.nFlag = result&0x8000 != 0
+	cpu.zFlag = result == 0
+	// Unsigned carry
+	cpu.cFlag = cpu.getXRegister() >= data
+}
+
+// cpx8 does a 8bit comparison of the X register with the data
+func (cpu *CPU) cpx8(data uint8) {
+	result := cpu.getXLRegister() - data
+
+	// Last bit value
+	cpu.nFlag = result&0x80 != 0
+	cpu.zFlag = result == 0
+	// Unsigned carry
+	cpu.cFlag = cpu.getXLRegister() >= data
+}
+
+// cpx compare the X register to the data handling the 16bit/8bit distinction
+func (cpu *CPU) cpx(dataHi, dataLo uint8) {
+	if cpu.xFlag {
+		cpu.cpx8(dataLo)
+	} else {
+		cpu.cpx16(utils.JoinUint16(dataHi, dataLo))
+	}
+}
+
+func (cpu *CPU) opE0() {
+	dataHi, dataLo := cpu.admImmediateX()
+	cpu.cpx(dataHi, dataLo)
+	cpu.cycles += 3 - utils.BoolToUint16[cpu.xFlag]
+}
+
+func (cpu *CPU) opE4() {
+	dataHi, dataLo := cpu.admDirect()
+	cpu.cpx(dataHi, dataLo)
+	cpu.cycles += 4 - utils.BoolToUint16[cpu.xFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) opEC() {
+	dataHi, dataLo := cpu.admAbsolute()
+	cpu.cpx(dataHi, dataLo)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.xFlag]
+}
+
+// cpy16 does a 16bit comparison of the Y register with the data
+func (cpu *CPU) cpy16(data uint16) {
+	result := cpu.getYRegister() - data
+
+	// Last bit value
+	cpu.nFlag = result&0x8000 != 0
+	cpu.zFlag = result == 0
+	// Unsigned carry
+	cpu.cFlag = cpu.getYRegister() >= data
+}
+
+// cpy8 does a 8bit comparison of the Y register with the data
+func (cpu *CPU) cpy8(data uint8) {
+	result := cpu.getYLRegister() - data
+
+	// Last bit value
+	cpu.nFlag = result&0x80 != 0
+	cpu.zFlag = result == 0
+	// Unsigned carry
+	cpu.cFlag = cpu.getYLRegister() >= data
+}
+
+// cpy compare the Y register to the data handling the 16bit/8bit distinction
+func (cpu *CPU) cpy(dataHi, dataLo uint8) {
+	if cpu.xFlag {
+		cpu.cpy8(dataLo)
+	} else {
+		cpu.cpy16(utils.JoinUint16(dataHi, dataLo))
+	}
+}
+
+func (cpu *CPU) opC0() {
+	dataHi, dataLo := cpu.admImmediateX()
+	cpu.cpy(dataHi, dataLo)
+	cpu.cycles += 3 - utils.BoolToUint16[cpu.xFlag]
+}
+
+func (cpu *CPU) opC4() {
+	dataHi, dataLo := cpu.admDirect()
+	cpu.cpy(dataHi, dataLo)
+	cpu.cycles += 4 - utils.BoolToUint16[cpu.xFlag] + utils.BoolToUint16[cpu.getDLRegister() == 0]
+}
+
+func (cpu *CPU) opCC() {
+	dataHi, dataLo := cpu.admAbsolute()
+	cpu.cpy(dataHi, dataLo)
+	cpu.cycles += 5 - utils.BoolToUint16[cpu.xFlag]
+}
