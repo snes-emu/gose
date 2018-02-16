770702b941c88e9f5c18f60fa5497e35341c97d1
Tabet Sami <salph.tabet@gmail.com>
2018-02-16 20:17:53 +0100

    Refactor(compare_test): refactored compare tests and added two tests

--- cpu/op_compare_test.go
+++ cpu/op_compare_test.go
@@ -2,27 +2,38 @@ package cpu
 
 import "testing"
 
-func TestCmpExample1(t *testing.T) {
-	cpu := &CPU{
-		C:     0x1234,
-		mFlag: false,
-		dFlag: false,
-		cFlag: false,
+func TestCmp(t *testing.T) {
+
+	testCases := []struct {
+		value          *CPU
+		expected       CPU
+		dataHi, dataLo uint8
+		operator       func(uint8, uint8)
+	}{
+		{
+			value:    &CPU{C: 0x1234},
+			expected: CPU{C: 0x1234, zFlag: true, cFlag: true},
+			dataHi:   0x12, dataLo: 0x34,
+		},
+		{
+			value:    &CPU{C: 0x1104, mFlag: true},
+			expected: CPU{C: 0x1104, mFlag: true, zFlag: true, cFlag: true},
+			dataHi:   0x00, dataLo: 0x04,
+		},
+		{
+			value:    &CPU{C: 0x1103, mFlag: true},
+			expected: CPU{C: 0x1103, mFlag: true, nFlag: true},
+			dataHi:   0x00, dataLo: 0x04,
+		},
 	}
 
-	cpu.cmp(0x12, 0x34)
+	for i, tc := range testCases {
+		tc.value.cmp(tc.dataHi, tc.dataLo)
 
-	cpu2 := CPU{
-		C:     0x1234,
-		nFlag: false,
-		zFlag: true,
-		vFlag: false,
-		cFlag: true,
-	}
+		err := tc.value.compare(tc.expected)
 
-	err := cpu.compare(cpu2)
-
-	if err != nil {
-		t.Error(err)
+		if err != nil {
+			t.Errorf("Test %v failed: \n%v", i, err)
+		}
 	}
 }
