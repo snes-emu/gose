eac78e23336ce37d2f99ae892e66057fb693aff4
Tabet Sami <salph.tabet@gmail.com>
2018-02-16 19:41:58 +0100

    Feature(op_carry): added one more test for adc and one more test for sbc

--- cpu/op_carry_test.go
+++ cpu/op_carry_test.go
@@ -27,6 +27,32 @@ func TestAdcExample1(t *testing.T) {
 	}
 }
 
+func TestAdcExample2(t *testing.T) {
+	cpu := &CPU{
+		C:     0x00ff,
+		mFlag: true,
+		dFlag: false,
+		cFlag: true,
+	}
+
+	cpu.adc(0x00, 0x06)
+
+	cpu2 := CPU{
+		C:     0x0006,
+		mFlag: true,
+		nFlag: false,
+		zFlag: false,
+		vFlag: false,
+		cFlag: false,
+	}
+
+	err := cpu.compare(cpu2)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
 func TestSbcExample1(t *testing.T) {
 	cpu := &CPU{
 		C:     0x0001,
@@ -52,26 +78,26 @@ func TestSbcExample1(t *testing.T) {
 	}
 }
 
-// func TestSbcExample2(t *testing.T) {
-// 	cpu := &CPU{
-// 		C:     0x0001,
-// 		mFlag: false,
-// 		dFlag: true,
-// 		cFlag: true,
-// 	}
+func TestSbcExample2(t *testing.T) {
+	cpu := &CPU{
+		C:     0x0002,
+		mFlag: true,
+		cFlag: true,
+	}
 
-// 	cpu.sbc(0x20, 0x03)
+	cpu.sbc(0x00, 0x03)
 
-// 	cpu2 := CPU{
-// 		C:     0x7998,
-// 		nFlag: false,
-// 		zFlag: false,
-// 		cFlag: false,
-// 	}
+	cpu2 := CPU{
+		C:     0x00ff,
+		mFlag: true,
+		nFlag: true,
+		zFlag: false,
+		cFlag: false,
+	}
 
-// 	err := cpu.compare(cpu2)
+	err := cpu.compare(cpu2)
 
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-// }
+	if err != nil {
+		t.Error(err)
+	}
+}
