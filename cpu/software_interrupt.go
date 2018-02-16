6fa1e11f3eea61024eed556a0a7059e39c07ca6e
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-16 14:05:13 +0100

    refactor(software_interrupt): merge brk,cop instruction files

--- cpu/software_interrupt.go
+++ cpu/software_interrupt.go
@@ -30,3 +30,29 @@ func (cpu *CPU) op00() {
 	cpu.cycles += 8 - utils.BoolToUint16[cpu.eFlag]
 
 }
+
+func (cpu *CPU) op02() {
+	addressHi, addressLo := utils.SplitUint16(cpu.getPCRegister() + 2)
+	if cpu.eFlag {
+		cpu.pushStack(addressHi)
+		cpu.pushStack(addressLo)
+		cpu.php()
+		cpu.K = 0x00
+		addressLo := cpu.memory.GetByteBank(0x00, 0xFFF4)
+		addressHi := cpu.memory.GetByteBank(0x00, 0xFFF5)
+		cpu.PC = utils.JoinUint16(addressHi, addressLo)
+	} else {
+		cpu.pushStack(cpu.getKRegister())
+		cpu.pushStack(addressHi)
+		cpu.pushStack(addressLo)
+		cpu.php()
+		cpu.K = 0x00
+		addressLo := cpu.memory.GetByteBank(0x00, 0xFFE4)
+		addressHi := cpu.memory.GetByteBank(0x00, 0xFFE5)
+		cpu.PC = utils.JoinUint16(addressHi, addressLo)
+	}
+	cpu.dFlag = false
+	cpu.iFlag = true
+	cpu.cycles += 8 - utils.BoolToUint16[cpu.eFlag]
+
+}
