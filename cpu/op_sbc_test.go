89c314f9be2271befa37c1ecd46430b95979b569
Tabet Sami <salph.tabet@gmail.com>
2018-02-16 12:59:03 +0100

    Refactor(op_sbc_test): now use the compare method for testing

--- cpu/op_sbc_test.go
+++ cpu/op_sbc_test.go
@@ -3,42 +3,50 @@ package cpu
 import "testing"
 
 func TestSbcExample1(t *testing.T) {
-	cpu := New()
-
-	cpu.setCRegister(0x0001)
-	cpu.mFlag = false
-	cpu.dFlag = false
-	cpu.cFlag = true
+	cpu := &CPU{
+		C:     0x0001,
+		mFlag: false,
+		dFlag: false,
+		cFlag: true,
+	}
 
 	cpu.sbc(0x20, 0x03)
 
-	success := cpu.getCRegister() == 0xdffe &&
-		cpu.nFlag &&
-		!cpu.vFlag &&
-		!cpu.zFlag &&
-		!cpu.cFlag
+	cpu2 := CPU{
+		C:     0xdffe,
+		nFlag: true,
+		zFlag: false,
+		vFlag: false,
+		cFlag: false,
+	}
 
-	if !success {
-		t.Error("Example 1 fails on the accumulator (decimal mode off)")
+	err := cpu.compare(cpu2)
+
+	if err != nil {
+		t.Error(err)
 	}
 }
 
 func TestSbcExample2(t *testing.T) {
-	cpu := New()
-
-	cpu.setCRegister(0x0001)
-	cpu.mFlag = false
-	cpu.dFlag = true
-	cpu.cFlag = true
+	cpu := &CPU{
+		C:     0x0001,
+		mFlag: false,
+		dFlag: true,
+		cFlag: true,
+	}
 
 	cpu.sbc(0x20, 0x03)
 
-	success := cpu.getCRegister() == 0x7998 &&
-		!cpu.nFlag &&
-		!cpu.zFlag &&
-		!cpu.cFlag
+	cpu2 := CPU{
+		C:     0x7998,
+		nFlag: false,
+		zFlag: false,
+		cFlag: false,
+	}
 
-	if !success {
-		t.Error("Example 2 fails on the accumulator (decimal mode on)")
+	err := cpu.compare(cpu2)
+
+	if err != nil {
+		t.Error(err)
 	}
 }
