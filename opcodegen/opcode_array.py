739bc717b44ee9614acc95a746e28ab3a8f6b3db
Tabet Sami <salph.tabet@gmail.com>
2018-02-17 20:08:04 +0100

    Fix(opcode_array): cleaner way of generating opcodes list

--- opcodegen/opcode_array.py
+++ opcodegen/opcode_array.py
@@ -1,10 +1,13 @@
 ARRAY_NAME = "cpu.opcodes"
 ARRAY_SIZE = 256
 
+
 def opcode_array_filler():
-    for op in range(ARRAY_SIZE):
-        print('{0}[0x{1:X}]=cpu.op{1:X}'.format(ARRAY_NAME, op))
+    return '\n'.join(
+        ['{0}[0x{1:X}]=cpu.op{1:X}'.format(ARRAY_NAME, op) for op in range(ARRAY_SIZE)
+         ]
+    )
 
 
 if __name__ == '__main__':
-    opcode_array_filler()
+    print(opcode_array_filler())
