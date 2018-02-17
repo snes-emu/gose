bb5f3ec31221d228d66cc8a175bfd75bc0c7e9ce
Tabet Sami <salph.tabet@gmail.com>
2018-02-17 19:52:06 +0100

    Feature(op_compare): added test cases for cpx operation

--- cpu/op_compare_test.go
+++ cpu/op_compare_test.go
@@ -37,3 +37,39 @@ func TestCmp(t *testing.T) {
 		}
 	}
 }
+
+func TestCpx(t *testing.T) {
+
+	testCases := []struct {
+		value          *CPU
+		expected       CPU
+		dataHi, dataLo uint8
+		operator       func(uint8, uint8)
+	}{
+		{
+			value:    &CPU{X: 0x1234},
+			expected: CPU{X: 0x1234, zFlag: true, cFlag: true},
+			dataHi:   0x12, dataLo: 0x34,
+		},
+		{
+			value:    &CPU{X: 0x0004, xFlag: true},
+			expected: CPU{X: 0x0004, xFlag: true, zFlag: true, cFlag: true},
+			dataHi:   0x00, dataLo: 0x04,
+		},
+		{
+			value:    &CPU{X: 0x0003, xFlag: true},
+			expected: CPU{X: 0x0003, xFlag: true, nFlag: true},
+			dataHi:   0x00, dataLo: 0x04,
+		},
+	}
+
+	for i, tc := range testCases {
+		tc.value.cpx(tc.dataHi, tc.dataLo)
+
+		err := tc.value.compare(tc.expected)
+
+		if err != nil {
+			t.Errorf("Test %v failed: \n%v", i, err)
+		}
+	}
+}
