b28c4fcc28c624dc487964f22d7622c7d56d8cbe
Tabet Sami <salph.tabet@gmail.com>
2018-02-17 23:35:28 +0100

    Feature(op_bit): added tests for bit operation

---
+++ cpu/op_bit_test.go
@@ -0,0 +1,38 @@
+package cpu
+
+import (
+	"testing"
+)
+
+func TestBit(t *testing.T) {
+
+	testCases := []struct {
+		expected       CPU
+		value          *CPU
+		dataHi, dataLo uint8
+		immediate      bool
+		operator       func(uint8, uint8)
+	}{
+		{
+			value:    &CPU{C: 0x0043, DBR: 0x12, mFlag: true},
+			expected: CPU{C: 0x0043, DBR: 0x12, mFlag: true, nFlag: true, zFlag: true},
+			dataHi:   0x00, dataLo: 0x9c,
+		},
+		{
+			value:    &CPU{C: 0xabff, nFlag: true, vFlag: true, zFlag: true},
+			expected: CPU{C: 0xabff, nFlag: true, vFlag: true, zFlag: false},
+			dataHi:   0x00, dataLo: 0x06,
+			immediate: true,
+		},
+	}
+
+	for i, tc := range testCases {
+		tc.value.bit(tc.dataHi, tc.dataLo, tc.immediate)
+
+		err := tc.value.compare(tc.expected)
+
+		if err != nil {
+			t.Errorf("Test %v failed: \n%v", i, err)
+		}
+	}
+}
