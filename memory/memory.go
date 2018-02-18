2f951ecf1cff286d1202087bea4993174710b462
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-02-18 01:05:35 +0100

    feature(memory): add sram and wram to memory

--- memory/memory.go
+++ memory/memory.go
@@ -2,31 +2,35 @@ package memory
 
 const bankNumber = 256
 const offsetMask = 0xFFFF
+const sramSize = 0x8000
+const wramSize = 0x20000
 
 type Memory struct {
-	bank [bankNumber][]uint8
+	main [bankNumber][]uint8
+	sram [sramSize]uint8
+	wram [wramSize]uint8
 }
 
 func New() *Memory {
 	memory := &Memory{}
 	for bank := 0; bank < bankNumber; bank++ {
-		memory.bank[bank] = make([]byte, 1<<16)
+		memory.main[bank] = make([]byte, 1<<16)
 	}
 	return memory
 }
 
 func (memory Memory) GetByte(index uint32) uint8 {
-	return memory.bank[index>>16][index&offsetMask]
+	return memory.main[index>>16][index&offsetMask]
 }
 
 func (memory Memory) GetByteBank(K uint8, address uint16) uint8 {
-	return memory.bank[K][address]
+	return memory.main[K][address]
 }
 
 func (memory Memory) SetByte(value uint8, index uint32) {
-	memory.bank[index>>16][index&offsetMask] = value
+	memory.main[index>>16][index&offsetMask] = value
 }
 
 func (memory Memory) SetByteBank(value uint8, K uint8, address uint16) {
-	memory.bank[K][address] = value
+	memory.main[K][address] = value
 }
