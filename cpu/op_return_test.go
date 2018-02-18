40eef53c421de50ba696428ec6a5069370ab6a82
Tabet Sami <salph.tabet@gmail.com>
2018-02-18 22:04:43 +0100

    Test(op_return: dded test case for rti operation

--- cpu/op_return_test.go
+++ cpu/op_return_test.go
@@ -30,3 +30,30 @@ func TestRts(t *testing.T) {
 		}
 	}
 }
+
+func TestRti(t *testing.T) {
+	mem := memory.New()
+	mem.SetByteBank(0x08, 0x00, 0x01fc)
+	mem.SetByteBank(0x12, 0x00, 0x01fd)
+	mem.SetByteBank(0x34, 0x00, 0x01fe)
+	mem.SetByteBank(0x56, 0x00, 0x01ff)
+	testCases := []struct {
+		value    *CPU
+		expected CPU
+	}{
+		{
+			value:    &CPU{S: 0x01fb, memory: mem},
+			expected: CPU{S: 0x01ff, K: 0x56, PC: 0x3412, dFlag: true, memory: mem},
+		},
+	}
+
+	for i, tc := range testCases {
+		tc.value.rti()
+
+		err := tc.value.compare(tc.expected)
+
+		if err != nil {
+			t.Errorf("Test %v failed: \n%v", i, err)
+		}
+	}
+}
