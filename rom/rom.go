60f8e31c024eb5be882aa17289a5f381ab6f8b60
Tabet Sami <salph.tabet@gmail.com>
2018-02-22 23:01:31 +0100

    Feature(rom): added rom title parsing to the rom package

--- rom/rom.go
+++ rom/rom.go
@@ -1,19 +1,45 @@
 package rom
 
+import "fmt"
+
 type ROM struct {
-	rawData   []byte
-	GameTitle string
+	data  []byte
+	Title string
 }
 
 // ParseROM parses a ROM file representation in bytes and return a representation
-func ParseROM(data []byte) *ROM {
+func ParseROM(data []byte) (*ROM, error) {
 	rom := &ROM{
-		rawData: data,
+		data: data,
 	}
 
-	title := data[0xfc0:0xfd4]
+	// SMC header should be of len 0 or 512
+	smcHeaderSize := len(rom.data) % 0x400
+	if smcHeaderSize != 0 && smcHeaderSize != 512 {
+		return nil, fmt.Errorf("The smc header of this rom is not conventional (len: %v)", smcHeaderSize)
+	}
 
-	rom.GameTitle = string(title)
+	// Remove smc header
+	rom.data = rom.data[smcHeaderSize:]
 
-	return rom
+	// Set rom Title
+	if rom.isLo() {
+		rom.Title = string(rom.data[0x7fc0:0x7fd4])
+	} else {
+		rom.Title = string(rom.data[0xffc0:0xffd4])
+	}
+
+	return rom, nil
+}
+
+// isLo checks if the ROM is of type LoROM
+func (rom ROM) isLo() bool {
+	for _, c := range rom.data[0x7fc0:0x7fd4] {
+		// check if all chars are ascii characters
+		if c > 0x7f || c < 0x1f {
+			return false
+		}
+	}
+
+	return true
 }
