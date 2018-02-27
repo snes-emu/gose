cbebf62a4245c09f20918d86a5e4e886f6545625
Sami Tabet <salph.tabet@gmail.com>
2018-02-28 00:26:35 +0100

    Feature(ppu): added needed attributes for oam registers in the ppu struct

--- ppu/ppu.go
+++ ppu/ppu.go
@@ -6,8 +6,14 @@ type PPU struct {
 	oam       [0x200 + 0x20]byte // oam represents the object attribute memory (512 + 32 Bytes)
 	cgram     [0x200]byte        // cgram represents the color graphics ram and stores the color palette with 256 color entries
 	registers [0x40]register     // registers represents the ppu registers as methods
-	cgramAddr uint16             // store the cgram address over 512 byte (not the Word addr !)
-	cgramLsb  uint8              // temporary variable for the cgdata register
+
+	cgramAddr uint16 // store the cgram address over 512 byte (not the Word addr !)
+	cgramLsb  uint8  // temporary variable for the cgdata register
+
+	oamAddr            uint16 // the OAM addr p------b aaaaaaaa (p is the Obj Priority activation bit and the rest represents the oam addr)
+	oamLastWrittenAddr uint16 // variable to hold the last written oamAddr
+	oamFlip            uint16 // Hold addr flip (even or odd part of a word)
+	oamLsb             uint8  // temporary variable for the oamdata register
 }
 
 type register func(uint8) uint8
