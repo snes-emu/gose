5defd0e1deecbbd5acbd2349f87b101fc9e14845
Sami Tabet <salph.tabet@gmail.com>
2018-02-28 23:52:20 +0100

    Test(oam): added a test for oam registers

---
+++ ppu/oam_test.go
@@ -0,0 +1,25 @@
+package ppu
+
+import (
+	"testing"
+)
+
+func TestOam(t *testing.T) {
+	// Set oam addr register to 0x104
+	ppu := &PPU{}
+	ppu.oamaddl(0x04)
+	ppu.oamaddh(0x1)
+
+	// Write 4 bytes
+	ppu.oamdata(1)
+	ppu.oamdata(2)
+	ppu.oamdata(7)
+	ppu.oamdata(12)
+
+	// Write 1 to 0x2103
+	ppu.oamaddh(0x1)
+
+	if ppu.oamLastWrittenAddr%512 != 2*4 {
+		t.Errorf("Wrong value for internal oam address, expected 8, got: %v", ppu.oamLastWrittenAddr)
+	}
+}
