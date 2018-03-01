814fde3f848f3aebc887f4e487b0cf3ccab0f2ae
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-03-01 17:29:23 +0100

    Feature(ppu): add BGMODE register

--- ppu/bg.go
+++ ppu/bg.go
@@ -1,11 +1,23 @@
 package ppu
 
+// BG stores data about a background
 type BG struct {
-	tileSize           bool
-	mosaic             bool
-	size               uint8
-	tileMapBaseAddress uint16
-	tileSetBaseAddress uint16
-	horizontalScroll   uint16
-	verticalScroll     uint16
+	TileSize           bool   // false 8x8 tiles, true 16x16 tiles
+	Mosaic             bool   // mosaic mode enabled
+	Priority           bool   // Only useful for BG3
+	screenSize         uint8  // 0=32x32, 1=64x32, 2=32x64, 3=64x64 tiles
+	tileMapBaseAddress uint16 // base address for tile map in VRAM
+	tileSetBaseAddress uint16 // base address for tile set in VRAM
+	horizontalScroll   uint16 // horizontal scroll in pixel
+	verticalScroll     uint16 // vertical scroll in pixel
+}
+
+// 2105h - BGMODE - BG Mode and BG Character Size (W)
+func (ppu *PPU) bgmode(data uint8) {
+	ppu.bgScreenMode = data & 7
+	ppu.bg[2].Priority = data&8 != 0
+	for i := uint8(0); i < 4; i++ {
+		ppu.bg[i].TileSize = data&(1<<4+i) != 0
+	}
+
 }
