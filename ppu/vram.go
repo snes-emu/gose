a975cb2e09c4f64fb08f260b423bb71c1848f5c2
Sami Tabet <salph.tabet@gmail.com>
2018-03-04 01:47:32 +0100

    Feature(vram): added a vramAddrTranslation method to ppu

--- ppu/vram.go
+++ ppu/vram.go
@@ -1,5 +1,28 @@
 package ppu
 
+import "fmt"
+
+// vramAddrTranslation Performs the address translation
+func (ppu PPU) vramAddrTranslation(addr uint16) uint16 {
+	switch ppu.vramAddrMapping {
+	case 0x0:
+		// No remapping
+		return addr
+	case 0x1:
+		// Remap addressing aaaaaaaaBBBccccc => aaaaaaaacccccBBB
+		return (addr & 0xff00) | ((addr & 0xe0) >> 5) | ((addr & 0x1f) << 3)
+	case 0x2:
+		// Remap addressing aaaaaaaBBBcccccc => aaaaaaaccccccBBB
+		return (addr & 0xfe00) | ((addr & 0x1c0) >> 6) | ((addr & 0x3F) << 3)
+	case 0x3:
+		// Remap addressing aaaaaaBBBccccccc => aaaaaacccccccBBB
+		return (addr & 0xfc00) | ((addr & 0xb80) >> 7) | ((addr & 0x7F) << 3)
+
+	default:
+		panic(fmt.Sprintf("Unknown vram Addr mapping mode: %v", ppu.vramAddrMapping))
+	}
+}
+
 // 2115 - VMAIN - VRAM Address Increment Mode (W)
 func (ppu *PPU) vmain(data uint8) uint8 {
 	ppu.vramIncrementMode = data&0x80 != 0
