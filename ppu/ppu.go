ad301f1633f066ec82fc3ae4cc3bfc80b6a2c4c8
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-03-06 14:17:46 +0100

    Fix(ppu): struct cleanup

--- ppu/ppu.go
+++ ppu/ppu.go
@@ -2,15 +2,14 @@ package ppu
 
 // PPU represents the Picture Processing Unit of the SNES
 type PPU struct {
-	vram      *vram          // vram represents the VideoRAM (64KB)
-	oam       *oam           // oam represents the object attribute memory (512 + 32 Bytes)
-	cgram     *cgram         // cgram represents the color graphics ram and stores the color palette with 256 color entries
-	registers [0x40]register // registers represents the ppu registers as methods
-
+	vram           *vram           // vram represents the VideoRAM (64KB)
+	oam            *oam            // oam represents the object attribute memory (512 + 32 Bytes)
+	cgram          *cgram          // cgram represents the color graphics ram and stores the color palette with 256 color entries
 	backgroundData *backgroundData // background data
 	colorMath      *colorMath      // Color math parameters
+	m7             *m7             // mode 7 parameters
 
-	m7 *m7 // mode 7 parameters
+	registers [0x40]register // registers represents the ppu registers as methods
 }
 
 type register func(uint8) uint8
