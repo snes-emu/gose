87ad82766578bdff526cd1d17fd481836d3cdba1
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-03-06 14:12:42 +0100

    Feature(ppu-cgram): move cgram parameters in a dedictated struct

--- ppu/ppu.go
+++ ppu/ppu.go
@@ -4,11 +4,8 @@ package ppu
 type PPU struct {
 	vram      *vram          // vram represents the VideoRAM (64KB)
 	oam       *oam           // oam represents the object attribute memory (512 + 32 Bytes)
-	cgram     [0x200]byte    // cgram represents the color graphics ram and stores the color palette with 256 color entries
+	cgram     *cgram         // cgram represents the color graphics ram and stores the color palette with 256 color entries
 	registers [0x40]register // registers represents the ppu registers as methods
-
-	cgramAddr uint16 // store the cgram address over 512 byte (not the Word addr !)
-	cgramLsb  uint8  // temporary variable for the cgdata register
 
 	backgroundData *backgroundData // background data
 	colorMath      *colorMath      // Color math parameters
