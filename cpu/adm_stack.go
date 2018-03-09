24011fe06a44fe450dde70bb311ea3249db2c152
Sami Tabet <salph.tabet@gmail.com>
2018-03-10 00:39:02 +0100

    Refactor(adm_stack): little endian convention

--- cpu/adm_stack.go
+++ cpu/adm_stack.go
@@ -4,8 +4,8 @@ import "github.com/snes-emu/gose/utils"
 
 // STACK,S addressing mode
 func (cpu CPU) admStackS() (uint8, uint8) {
-	haddress, laddress := cpu.admStackSP()
-	return cpu.memory.GetByte(haddress), cpu.memory.GetByte(laddress)
+	laddress, haddress := cpu.admStackSP()
+	return cpu.memory.GetByte(laddress), cpu.memory.GetByte(haddress)
 }
 
 // STACK,S addressing mode pointer
@@ -13,13 +13,13 @@ func (cpu CPU) admStackSP() (uint32, uint32) {
 	LL := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+1)
 	laddress := uint32(cpu.getSRegister() + uint16(LL))
 	haddress := uint32(cpu.getSRegister() + uint16(LL) + 1)
-	return haddress, laddress
+	return laddress, haddress
 }
 
 // (STACK,S),Y addressing mode
 func (cpu CPU) admPStackSY() (uint8, uint8) {
-	haddr, laddr := cpu.admPStackSYP()
-	return cpu.memory.GetByte(haddr), cpu.memory.GetByte(laddr)
+	laddr, haddr := cpu.admPStackSYP()
+	return cpu.memory.GetByte(laddr), cpu.memory.GetByte(haddr)
 }
 
 // (STACK,S),Y addressing mode
@@ -30,5 +30,5 @@ func (cpu CPU) admPStackSYP() (uint32, uint32) {
 	ll := cpu.memory.GetByte(laddress)
 	hh := cpu.memory.GetByte(haddress)
 	pointer := utils.JoinUint32(ll, hh, cpu.getDBRRegister()) + uint32(cpu.getYRegister())
-	return pointer + 1, pointer
+	return pointer, pointer + 1
 }
