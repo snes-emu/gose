f654e47b7612f906fb8112ffd80fc77b9fac84ce
Sami Tabet <salph.tabet@gmail.com>
2018-03-10 00:35:16 +0100

    Refactor(adm_accumulator): little endian convention

--- cpu/adm_accumulator.go
+++ cpu/adm_accumulator.go
@@ -2,5 +2,5 @@ package cpu
 
 // ACCUMULATOR addressing mode
 func (cpu CPU) admAccumulator() (uint8, uint8) {
-	return cpu.getBRegister(), cpu.getARegister()
+	return cpu.getARegister(), cpu.getBRegister()
 }
