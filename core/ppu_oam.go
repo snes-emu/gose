124f63de765916b14eedbdaf15f56ada1d48ff69
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-03-11 20:49:48 +0100

    Feature(oam): add sprite size conversion table

--- core/ppu_oam.go
+++ core/ppu_oam.go
@@ -62,3 +62,22 @@ func (ppu *PPU) obsel(data uint8) {
 	ppu.oam.objectTileBaseAddress = uint16(data&0x7) << 14
 	ppu.oam.objectTileGapAddress = uint16((data>>3)&0x3) << 13
 }
+
+var spriteSizeTable = [16][2]uint8{
+	{8, 8},
+	{8, 8},
+	{8, 8},
+	{16, 16},
+	{16, 16},
+	{32, 32},
+	{16, 32},
+	{16, 32},
+	{16, 16},
+	{32, 32},
+	{64, 64},
+	{32, 32},
+	{64, 64},
+	{64, 64},
+	{32, 64},
+	{32, 32},
+}
