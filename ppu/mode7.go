f968cd3dbb65101bf4e602dd059046e85de8755a
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-03-11 16:04:42 +0100

    Feature(ppu-m7): add signed multiply registers

--- ppu/mode7.go
+++ ppu/mode7.go
@@ -1,5 +1,9 @@
 package ppu
 
+import (
+	"github.com/snes-emu/gose/utils"
+)
+
 type m7 struct {
 	verticalFlip                   bool   // Vertical flip flag used in mode7
 	horizontalFlip                 bool   // Horizontal flip flag used in mode7
@@ -8,6 +12,7 @@ type m7 struct {
 	aParam, bParam, cParam, dParam uint16 // Rotation/scaling parameters used in mode 7
 	hofsParam, vofsParam           uint16 // Mode 7 horizontal and vertical scroll offset parameters
 	xParam, yParam                 uint16 // Mode 7 Center Coordinate parameters
+	signedMutlResult               uint32 // 24-bit result of the product A*(B>>8)
 }
 
 // 211Ah - M7SEL - Rotation/Scaling Mode Settings (W)
@@ -23,6 +28,7 @@ func (ppu *PPU) m7a(data uint8) uint8 {
 	data16 := uint16(data)
 	ppu.m7.aParam = (data16 << 8) | ppu.m7.cache
 	ppu.m7.cache = data16
+	ppu.m7.signedMutlResult = uint32(ppu.m7.aParam) * uint32(ppu.m7.bParam>>8)
 	return 0
 }
 
@@ -31,6 +37,7 @@ func (ppu *PPU) m7b(data uint8) uint8 {
 	data16 := uint16(data)
 	ppu.m7.bParam = (data16 << 8) | ppu.m7.cache
 	ppu.m7.cache = data16
+	ppu.m7.signedMutlResult = uint32(ppu.m7.aParam) * uint32(ppu.m7.bParam>>8)
 	return 0
 }
 
@@ -80,4 +87,22 @@ func (ppu *PPU) m7y(data uint8) uint8 {
 	ppu.m7.yParam = (data16 << 8) | ppu.m7.cache
 	ppu.m7.cache = data16
 	return 0
+}
+
+// 2134h - MPYL - Signed Multiply Result (lower 8bit) (R)
+func (ppu *PPU) mpyl(data uint8) uint8 {
+	LL, _, _ := utils.SplitUint32(ppu.m7.signedMutlResult)
+	return LL
+}
+
+// 2135h - MPYM - Signed Multiply Result (middle 8bit) (R)
+func (ppu *PPU) mpym(data uint8) uint8 {
+	_, MM, _ := utils.SplitUint32(ppu.m7.signedMutlResult)
+	return MM
+}
+
+// 2136h - MPYH - Signed Multiply Result (upper 8bit) (R)
+func (ppu *PPU) mpyh(data uint8) uint8 {
+	_, _, HH := utils.SplitUint32(ppu.m7.signedMutlResult)
+	return HH
 }
