de3b0de03c38d655e724c0fc366ab97b26bd85dd
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-03-12 16:59:32 +0100

    Fix(ppu): improve renderSpriteLine readability

--- ppu/render.go
+++ ppu/render.go
@@ -18,19 +18,23 @@ func (ppu PPU) renderSpriteLine() [HMax]pixel {
 		// Check if line intersects sprite
 		if ppu.vCounter >= sprite.y && ppu.vCounter < sprite.y+uint16(sprite.vSize) {
 			// Go through all tiles containing the line
-			for tile := uint16(sprite.y/8) << 4; tile < uint16(sprite.hSize)/8+uint16(sprite.y/8)<<4; tile++ {
+			baseTileIndex := uint16((ppu.vCounter-sprite.y)/8) << 4 // Y coordinate of the tile containing the line
+			for tile := baseTileIndex; tile < baseTileIndex+uint16(sprite.hSize)/8; tile++ {
 				// Go through all pixel in the line
-				for pixel := uint8(sprite.y % 8); pixel < uint8(sprite.y%8)+8; pixel++ {
+				for pixel := uint8(0); pixel < 8; pixel++ {
+					yPixel := uint16(ppu.vCounter-sprite.y) % 8                // Y coordinate of the line in the tile
+					lineBaseAddress := sprite.tileAddress + tile<<5 + 2*yPixel // Base address of the color planes of the pixel line in VRAM
 					// Get the color index of the pixel
 					colorIndex := uint8(0)
-					colorIndex += ppu.vram.bytes[sprite.tileAddress+tile<<5] & (1 << pixel) >> pixel
-					colorIndex += ppu.vram.bytes[sprite.tileAddress+tile<<5+0x01] & (1 << pixel) >> pixel << 1
-					colorIndex += ppu.vram.bytes[sprite.tileAddress+tile<<5+0x10] & (1 << pixel) >> pixel << 2
-					colorIndex += ppu.vram.bytes[sprite.tileAddress+tile<<5+0x11] & (1 << pixel) >> pixel << 3
+					colorIndex += ppu.vram.bytes[lineBaseAddress] & (1 << pixel) >> pixel
+					colorIndex += ppu.vram.bytes[lineBaseAddress+0x01] & (1 << pixel) >> pixel << 1
+					colorIndex += ppu.vram.bytes[lineBaseAddress+0x10] & (1 << pixel) >> pixel << 2
+					colorIndex += ppu.vram.bytes[lineBaseAddress+0x11] & (1 << pixel) >> pixel << 3
 					// If color is not tansparent, write color value in the pixel
 					if colorIndex != 0 {
-						pixels[sprite.x+8*uint16(tile)+uint16(pixel)].bgr = utils.JoinUint16(ppu.cgram.bytes[2*(128+16*sprite.paletteIndex+uint16(colorIndex))], ppu.cgram.bytes[2*(128+16*sprite.paletteIndex+uint16(colorIndex))+1])
-						pixels[sprite.x+8*uint16(tile)+uint16(pixel)].visible = true
+						colorAddress := 2 * (128 + 16*sprite.paletteIndex + uint16(colorIndex))
+						pixels[sprite.x+8*tile+uint16(pixel)].bgr = utils.JoinUint16(ppu.cgram.bytes[colorAddress], ppu.cgram.bytes[colorAddress+1])
+						pixels[sprite.x+8*tile+uint16(pixel)].visible = true
 					}
 				}
 
