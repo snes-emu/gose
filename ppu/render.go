be039d66e155d0b6ec70a39cad16b554799e523d
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-03-13 14:26:25 +0100

    Feature(render-sprite): implement 32-sprite limit and priority

--- ppu/render.go
+++ ppu/render.go
@@ -1,44 +1,60 @@
 package ppu
 
-import (
-	"github.com/snes-emu/gose/utils"
-)
+import "github.com/snes-emu/gose/utils"
 
 type pixel struct {
-	bgr     uint16
-	visible bool
+	bgr      uint16
+	visible  bool
+	priority uint8
 }
 
 func (ppu PPU) renderSpriteLine() [HMax]pixel {
 	// Initialize pixel line
 	var pixels [HMax]pixel
-	// Go through all sprites in reverse order
-	for i := uint16(127); i >= 0; i-- {
-		sprite := ppu.decodeSprite(i)
+	sprites := make([]uint16, 0, 32)
+	firstSprite := uint16(0)
+	if ppu.oam.priorityBit {
+		firstSprite = (ppu.oam.addr >> 2) & 0xFF
+	}
+	// Choose the first 32 sprites to appear on screen
+	for i := firstSprite; i < firstSprite+128; i++ {
 		// Check if line intersects sprite
-		if ppu.vCounter >= sprite.y && ppu.vCounter < sprite.y+uint16(sprite.vSize) {
-			// Go through all tiles containing the line
-			baseTileIndex := uint16((ppu.vCounter-sprite.y)/8) << 4 // Y coordinate of the tile containing the line
-			for tile := baseTileIndex; tile < baseTileIndex+uint16(sprite.hSize)/8; tile++ {
-				// Go through all pixel in the line
-				for pixel := uint8(0); pixel < 8; pixel++ {
-					yPixel := uint16(ppu.vCounter-sprite.y) % 8                // Y coordinate of the line in the tile
-					lineBaseAddress := sprite.tileAddress + tile<<5 + 2*yPixel // Base address of the color planes of the pixel line in VRAM
-					// Get the color index of the pixel
-					colorIndex := uint8(0)
-					colorIndex += ppu.vram.bytes[lineBaseAddress] & (1 << pixel) >> pixel
-					colorIndex += ppu.vram.bytes[lineBaseAddress+0x01] & (1 << pixel) >> pixel << 1
-					colorIndex += ppu.vram.bytes[lineBaseAddress+0x10] & (1 << pixel) >> pixel << 2
-					colorIndex += ppu.vram.bytes[lineBaseAddress+0x11] & (1 << pixel) >> pixel << 3
-					// If color is not tansparent, write color value in the pixel
-					if colorIndex != 0 {
-						colorAddress := 2 * (128 + 16*sprite.paletteIndex + uint16(colorIndex))
-						pixels[sprite.x+8*tile+uint16(pixel)].bgr = utils.JoinUint16(ppu.cgram.bytes[colorAddress], ppu.cgram.bytes[colorAddress+1])
-						pixels[sprite.x+8*tile+uint16(pixel)].visible = true
+		if ppu.vCounter >= uint16(ppu.oam.bytes[4*i+1]) && ppu.vCounter < uint16(ppu.oam.bytes[4*i+1])+uint16(spriteSizeTable[ppu.oam.objectSize|((ppu.oam.bytes[0x200+i/4]&(1<<(2*(i%4)+1)))>>(1<<(2*(i%4)+1)))<<4][1]) {
+			if len(sprites) == 32 {
+				ppu.status.rangeOver = true
+				break
+			} else {
+				sprites = append(sprites, i)
+			}
+		}
+	}
+	// Go through all the selected sprites in reverse order
+	for i := uint16(len(sprites) - 1); i >= 0; i-- {
+		sprite := ppu.decodeSprite(i)
+		// Go through all tiles containing the line
+		baseTileIndex := uint16((ppu.vCounter-sprite.y)/8) << 4 // Y coordinate of the tile containing the line
+		for tile := baseTileIndex; tile < baseTileIndex+uint16(sprite.hSize)/8; tile++ {
+			// Go through all pixel in the line
+			for pix := uint8(0); pix < 8; pix++ {
+				yPixel := uint16(ppu.vCounter-sprite.y) % 8                // Y coordinate of the line in the tile
+				lineBaseAddress := sprite.tileAddress + tile<<5 + 2*yPixel // Base address of the color planes of the pixel line in VRAM
+				// Get the color index of the pixel
+				colorIndex := uint8(0)
+				colorIndex += ppu.vram.bytes[lineBaseAddress] & (1 << pix) >> pix
+				colorIndex += ppu.vram.bytes[lineBaseAddress+0x01] & (1 << pix) >> pix << 1
+				colorIndex += ppu.vram.bytes[lineBaseAddress+0x10] & (1 << pix) >> pix << 2
+				colorIndex += ppu.vram.bytes[lineBaseAddress+0x11] & (1 << pix) >> pix << 3
+				// If color is not tansparent, write color value in the pixel
+				if colorIndex != 0 {
+					colorAddress := 2 * (128 + 16*sprite.paletteIndex + uint16(colorIndex))
+					pixels[sprite.x+8*tile+uint16(pix)] = pixel{
+						bgr:      utils.JoinUint16(ppu.cgram.bytes[colorAddress], ppu.cgram.bytes[colorAddress+1]),
+						visible:  true,
+						priority: sprite.priority,
 					}
 				}
-
 			}
+
 		}
 	}
 	return pixels
