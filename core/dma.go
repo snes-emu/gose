0d88792a69828bed997abebe4da9061da5c104e7
Sami Tabet <salph.tabet@gmail.com>
2018-04-08 15:05:42 +0200

    Feature(cpu): added dma channels

--- core/dma.go
+++ core/dma.go
@@ -21,3 +21,43 @@ type dmaChannel struct {
 	hdmaAddr        uint16
 	hdmaLineCounter uint8
 }
+
+func (cpu *CPU) initDma() {
+	for i := 0; i < 8; i++ {
+		cpu.dmaChannels[i] = &dmaChannel{
+			dmaEnabled:        false,
+			hdmaEnabled:       false,
+			transferDirection: true,
+			indirectMode:      true,
+			addressDecrement:  true,
+			fixedTransfer:     true,
+			transferMode:      7,
+			srcAddr:           0xffff,
+			srcBank:           0xff,
+			destAddr:          0xff,
+			transferSize:      0xffff,
+			indirectAddrBank:  0xff,
+			hdmaAddr:          0xffff,
+			hdmaLineCounter:   0xff,
+		}
+	}
+
+	cpu.Registers[0x20b] = cpu.mdmaen
+	cpu.Registers[0x20c] = cpu.hdmaen
+}
+
+// 0x420B - MDMAEN - Select General Purpose DMA Channel(s) and Start Transfer (W)
+func (cpu *CPU) mdmaen(x uint8) uint8 {
+	for i := uint8(0); i < 8; i++ {
+		cpu.dmaChannels[i].dmaEnabled = x&(1<<i) != 0
+	}
+	return 0
+}
+
+// 0x420C - HDMAEN - Select H-Blank DMA (H-DMA) Channel(s) (W)
+func (cpu *CPU) hdmaen(x uint8) uint8 {
+	for i := uint8(0); i < 8; i++ {
+		cpu.dmaChannels[i].hdmaEnabled = x&(1<<i) != 0
+	}
+	return 0
+}
