0c2002edbbf54ce3dfb3ae84449239760ade07f3
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-04-15 18:55:53 +0200

    Fix(adm): correct condition in relative8

--- core/addressing_modes.go
+++ core/addressing_modes.go
@@ -349,12 +349,11 @@ func (cpu CPU) admLongXP() (uint32, uint32) {
 // RELATIVE8 addressing mode
 func (cpu CPU) admRelative8() uint16 {
 	LL := cpu.memory.GetByteBank(cpu.getKRegister(), cpu.getPCRegister()+1)
-	switch t := LL >= 0; t {
-	case true:
+	if LL < 80 {
 		return uint16(LL)
-	default:
-		return uint16(LL) - 256
 	}
+	return uint16(LL) - 256
+
 }
 
 // RELATIVE16 addressing mode
