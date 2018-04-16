17c1b9b068d4a2351f0363404e33fc0652534974
Rémi Calixte <remi.calixte@student.ecp.fr>
2018-04-16 20:16:40 +0200

    Feature(utils): don't use buffers, greatly improve performance

--- utils/utils.go
+++ utils/utils.go
@@ -1,10 +1,5 @@
 package utils
 
-import (
-	"bytes"
-	"encoding/binary"
-)
-
 // BoolToUint16 provides a conversion from bool to uint16
 var BoolToUint16 = map[bool]uint16{
 	true:  1,
@@ -18,31 +13,17 @@ var BoolToUint8 = map[bool]uint8{
 }
 
 func JoinUint32(LL uint8, MM uint8, HH uint8) uint32 {
-	var ret uint32
-	buf := bytes.NewBuffer([]byte{LL, MM, HH, 0x00})
-	binary.Read(buf, binary.LittleEndian, &ret)
-	return ret
+	return uint32(LL) | uint32(MM)<<8 | uint32(HH)<<16
 }
 
 func JoinUint16(LL uint8, HH uint8) uint16 {
-	var ret uint16
-	buf := bytes.NewBuffer([]byte{LL, HH})
-	binary.Read(buf, binary.LittleEndian, &ret)
-	return ret
+	return uint16(LL) | uint16(HH)<<8
 }
 
 func SplitUint16(MM uint16) (uint8, uint8) {
-
-	buf := new(bytes.Buffer)
-	binary.Write(buf, binary.LittleEndian, &MM)
-	ret := buf.Bytes()
-	return ret[0], ret[1]
+	return uint8(MM), uint8(MM >> 8)
 }
 
 func SplitUint32(MM uint32) (uint8, uint8, uint8) {
-
-	buf := new(bytes.Buffer)
-	binary.Write(buf, binary.LittleEndian, &MM)
-	ret := buf.Bytes()
-	return ret[0], ret[1], ret[2]
+	return uint8(MM), uint8(MM >> 8), uint8(MM >> 16)
 }
