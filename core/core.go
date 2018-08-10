88f19a73c3fc4cbcfd5cf871e4943b69117f039f
Sami Tabet <sami.tabet@datadoghq.com>
2018-08-09 23:12:47 -0400

    Gose is now able to open zipped roms

--- core/core.go
+++ core/core.go
@@ -1,9 +1,11 @@
 package core
 
 import (
+	"archive/zip"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/snes-emu/gose/apu"
 	"github.com/snes-emu/gose/rom"
@@ -32,13 +34,47 @@ func New() *Emulator {
 	return &Emulator{cpu, mem, ppu}
 }
 
+func readFile(src string) ([]byte, error) {
+	r, err := zip.OpenReader(src)
+
+	if err != nil {
+		// Probably not a zip file try to open it as a .smc directly
+		f, err := os.Open(src)
+		defer f.Close()
+
+		if err != nil {
+			return nil, err
+		}
+
+		return ioutil.ReadAll(f)
+	}
+
+	defer r.Close()
+
+	var rom zip.File
+
+	for _, f := range r.File {
+		// Use the biggest file
+		if f.FileInfo().Size() > rom.FileInfo().Size() {
+			rom = *f
+		}
+	}
+
+	f, err := rom.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadAll(f)
+}
+
 func (e *Emulator) ReadROM(filename string) {
-	ROM, err := ioutil.ReadFile(filename)
+	buf, err := readFile(filename)
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
 
-	rom, err := rom.ParseROM(ROM)
+	rom, err := rom.ParseROM(buf)
 	fmt.Println(rom.Title)
 
 	if err != nil {
