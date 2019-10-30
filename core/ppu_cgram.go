814ed54b6e2fb77e7309b9433727d2e71d9bba5d
Rémi Calixte <remicalixte.rmc@gmail.com>
2019-10-30 16:01:53 +0100

    display color palette in debugger

--- core/ppu_cgram.go
+++ core/ppu_cgram.go
@@ -1,12 +1,19 @@
 package core
 
+import (
+	"encoding/json"
+	"image/color"
+)
+
+const cgramSize = 0x200
+
 // cgram represents the color graphics ram and stores the color palette with 256 color entries
 // Colors are stored in 2-byte words like:
 // _bbbbbgg gggrrrrr
 // The last bit is not used
 type cgram struct {
-	bytes [0x200]byte // bytes represents the raw 512 bytes (for 256 color entries)
-	addr  uint16      // store the cgram address over 512 byte (not the Word addr !)
+	bytes [cgramSize]byte // bytes represents the raw 512 bytes (for 256 color entries)
+	addr  uint16          // store the cgram address over 512 byte (not the Word addr !)
 	// lsb temporary variable for the cgdata register, it's used when we want to write a new color:
 	// - we first set the addr where we want to write (for instance the color number 67, we will have 61 * 2 = 122 stored in addr
 	// - we call cgdata to write once (this will store the provided data in the lsb)
@@ -50,4 +57,39 @@ func (cg *cgram) write(addr uint16, low uint8, high uint8) {
 
 func (cg *cgram) incrAddr() {
 	cg.addr = (cg.addr + 1) % 512
+}
+
+//ExportPalette exports the content of the cgram
+func (ppu *PPU) ExportPalette() color.Palette {
+	var palette color.Palette
+
+	for i := 0; i < cgramSize/2; i++ {
+		palette = append(palette, BGR555{uint16(ppu.cgram.bytes[2*i]) | uint16(ppu.cgram.bytes[2*i+1])<<8})
+	}
+	return palette
+}
+
+//BGR555 represents 16-bit opaque color, each channel uses 5 bits with red in the least significant bits
+type BGR555 struct {
+	color uint16
+}
+
+//RGBA implements the color.Color interface
+func (c BGR555) RGBA() (r, g, b, a uint32) {
+	a = 0xFFFF
+	r = uint32(c.color&0x1F) << 3
+	g = uint32((c.color&0x3E0)>>5) << 3
+	b = uint32((c.color&0x7C00)>>10) << 3
+
+	return
+}
+
+//MarshalJSON implements the json.Marshaler interface
+func (c BGR555) MarshalJSON() ([]byte, error) {
+	r, g, b, _ := c.RGBA()
+	return json.Marshal(map[string]uint32{
+		"r": r,
+		"g": g,
+		"b": b,
+	})
 }
