0211ca309bdb0738e508d2ecd30b19e7d6933d5b
Sami Tabet <salph.tabet@gmail.com>
2019-10-30 11:38:08 +0100

    ppu: refactor and fix oam

--- core/ppu_oam_test.go
+++ core/ppu_oam_test.go
@@ -1,10 +1,11 @@
 package core
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestOam(t *testing.T) {
+func TestOamLastWrittenAddr(t *testing.T) {
 	// Set oam addr register to 0x104
 	ppu := newPPU()
 	ppu.oamaddl(0x04)
@@ -22,4 +23,21 @@ func TestOam(t *testing.T) {
 	if ppu.oam.lastWrittenAddr%512 != 2*4 {
 		t.Errorf("Wrong value for internal oam address, expected 8, got: %v", ppu.oam.lastWrittenAddr)
 	}
+}
+
+func TestOamWritesAndReads(t *testing.T) {
+	// From: https://wiki.superfamicom.org/sprites
+	// Pictorially: Start OAM filled with all zeros.
+	// Write 1, read, read, Write 2, read, write 3
+	// => OAM is 00 00 01 02 01 03, rather than 01 00 00 02 00 03 as you might expect.
+
+	ppu := newPPU()
+	ppu.oamdata(0x1)
+	assert.EqualValues(t, 0, ppu.rdoam())
+	assert.EqualValues(t, 0, ppu.rdoam())
+	ppu.oamdata(0x2)
+	assert.EqualValues(t, 0, ppu.rdoam())
+	ppu.oamdata(0x3)
+
+	assert.EqualValues(t, []byte{0x00, 0x00, 0x01, 0x02, 0x01, 0x03}, ppu.oam.bytes[:6])
 }
