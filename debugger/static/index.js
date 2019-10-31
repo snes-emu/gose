53ef7f01c2f697eaa0a2aacc74f898914c3b99b3
Rémi Calixte <remicalixte.rmc@gmail.com>
2019-10-31 12:54:32 +0100

    add tabs

--- debugger/static/index.js
+++ debugger/static/index.js
@@ -1,15 +1,42 @@
 const paletteNum = 16
 const paletteSize = 16
-let palette = document.getElementById("palette");
 
+
+//tab management
+let container = document.getElementById('tab_container');
+
+let cpuTab = document.createElement("ul");
+cpuTab.id = 'cpu';
+
+let ppuTab = document.createElement('div');
+ppuTab.id = 'palette';
 for (let p = 0; p < paletteNum ; p++) {
     const row = document.createElement("div");
     row.appendChild(document.createTextNode(`${p}: `))
-    palette.appendChild(row);
+    ppuTab.appendChild(row);
     for (let c = 0; c < paletteSize; c++) {
-        palette.children[p].appendChild(document.createElement("div"))
+        ppuTab.children[p].appendChild(document.createElement("div"))
     }
 }
+
+
+let tabs = document.querySelectorAll('input[name=tab]');
+tabs.forEach(tab => tab.onchange= event => {
+    Array.from(container.children).forEach(child => container.removeChild(child));
+
+    switch (event.target.value) {
+        case 'cpu':
+            container.appendChild(cpuTab);
+            break;
+        case 'ppu':
+            container.appendChild(ppuTab);
+            break;
+    }
+
+});
+
+//mount initial tab
+container.appendChild(cpuTab);
 
 function pause() {
     fetch('/pause').then(resp => resp.json()).then(displayState);
@@ -25,14 +52,13 @@ function step() {
 function displayState(body) {
     const cpu = body.cpu;
 
-    const lu = document.getElementById("cpu");
     const li = document.createElement("li");
     li.appendChild(document.createTextNode(Object.keys(cpu).map(key => `${key}: ${cpu[key]}`).join(',')));
-    lu.appendChild(li);
+    cpuTab.appendChild(li);
 
     body.palette.forEach((color,i) => {
         const rgb = `rgb(${color.r},${color.g},${color.b})`;
-        palette.children[Math.floor(i/paletteNum)].children[i%paletteSize].style = `background-color:${rgb}`;
+        ppuTab.children[Math.floor(i/paletteNum)].children[i%paletteSize].style = `background-color:${rgb}`;
     });
 }
 
