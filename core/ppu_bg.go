d49e9c2372841d320e1d062cd217e12400b01105
Sami Tabet <salph.tabet@gmail.com>
2019-10-31 15:22:45 +0100

    ppu: add function to retrieve tiles from backgrounds

--- core/ppu_bg.go
+++ core/ppu_bg.go
@@ -14,22 +14,22 @@ type backgroundData struct {
 
 // BG stores data about a background
 type bg struct {
-	tileSize           bool   // false 8x8 tiles, true 16x16 tiles
-	mosaic             bool   // mosaic mode enabled
-	priority           bool   // Only useful for BG3
-	screenSize         uint8  // 0=32x32, 1=64x32, 2=32x64, 3=64x64 tiles
-	tileMapBaseAddress uint16 // base address for tile map in VRAM
-	tileSetBaseAddress uint16 // base address for tile set in VRAM
-	horizontalScroll   uint16 // horizontal scroll in pixel
-	verticalScroll     uint16 // vertical scroll in pixel
-	windowMask1        uint8  // mask for window 1 (0..1=Disable, 2=Inside, 3=Outside)
-	windowMask2        uint8  // mask for window 2 (0..1=Disable, 2=Inside, 3=Outside)
-	windowMaskLogic    uint8  // 0=OR, 1=AND, 2=XOR, 3=XNOR)
-	mainScreenWindow   bool   // Disable window area on main screen
-	subScreenWindow    bool   // Disable windows area on sub screen
-	mainScreen         bool   // Enable layer on main screen
-	subScreen          bool   // Enable layer on sub screen
-	colorMath          bool   // Flag to control colors on the BG (False: Display RAW Main Screen as such (without math), True: Apply math on Mainscreen)
+	tileSize         bool   // false 8x8 tiles, true 16x16 tiles
+	mosaic           bool   // mosaic mode enabled
+	priority         bool   // Only useful for BG3
+	screenSize       uint8  // 0=32x32, 1=64x32, 2=32x64, 3=64x64 tiles
+	tileMapBaseAddr  uint8  // base address for tile map in VRAM (in 1k word steps, 2k byte steps)
+	tileSetBaseAddr  uint8  // base address for tile set in VRAM (in 4k word steps, 8k byte steps)
+	horizontalScroll uint16 // horizontal scroll in pixel
+	verticalScroll   uint16 // vertical scroll in pixel
+	windowMask1      uint8  // mask for window 1 (0..1=Disable, 2=Inside, 3=Outside)
+	windowMask2      uint8  // mask for window 2 (0..1=Disable, 2=Inside, 3=Outside)
+	windowMaskLogic  uint8  // 0=OR, 1=AND, 2=XOR, 3=XNOR)
+	mainScreenWindow bool   // Disable window area on main screen
+	subScreenWindow  bool   // Disable windows area on sub screen
+	mainScreen       bool   // Enable layer on main screen
+	subScreen        bool   // Enable layer on sub screen
+	colorMath        bool   // Flag to control colors on the BG (False: Display RAW Main Screen as such (without math), True: Apply math on Mainscreen)
 }
 
 // 2105h - BGMODE - BG Mode and BG Character Size (W)
@@ -50,41 +50,48 @@ func (ppu *PPU) mosaic(data uint8) {
 	ppu.backgroundData.mosaicSize = data >> 4
 }
 
+// 2107h -  210Ah - BG?SC - BG? Screen Base and Screen Size (W)
+// 7-2  SC Base Address in VRAM (in 1K-word steps, aka 2K-byte steps)
+// 1-0  SC Size (0=One-Screen, 1=V-Mirror, 2=H-Mirror, 3=Four-Screen)
+// (0=32x32, 1=64x32, 2=32x64, 3=64x64 tiles)
+// (0: SC0 SC0    1: SC0 SC1  2: SC0 SC0  3: SC0 SC1   )
+// (   SC0 SC0       SC0 SC1     SC1 SC1     SC2 SC3   )
+func (bg *bg) bgsc(data uint8) {
+	bg.screenSize = data & 3
+	bg.tileMapBaseAddr = data >> 2
+}
+
 // 2107h - BG1SC - BG1 Screen Base and Screen Size (W)
 func (ppu *PPU) bg1sc(data uint8) {
-	ppu.backgroundData.bg[0].screenSize = data & 3
-	ppu.backgroundData.bg[0].tileMapBaseAddress = bit.JoinUint16(0x00, data&^uint8(3))
+	ppu.backgroundData.bg[0].bgsc(data)
 }
 
 // 2108h - BG2SC - BG2 Screen Base and Screen Size (W)
 func (ppu *PPU) bg2sc(data uint8) {
-	ppu.backgroundData.bg[1].screenSize = data & 3
-	ppu.backgroundData.bg[1].tileMapBaseAddress = bit.JoinUint16(0x00, data&^uint8(3))
+	ppu.backgroundData.bg[1].bgsc(data)
 }
 
 // 2109h - BG3SC - BG3 Screen Base and Screen Size (W)
 func (ppu *PPU) bg3sc(data uint8) {
-	ppu.backgroundData.bg[2].screenSize = data & 3
-	ppu.backgroundData.bg[2].tileMapBaseAddress = bit.JoinUint16(0x00, data&^uint8(3))
+	ppu.backgroundData.bg[2].bgsc(data)
 }
 
 // 210Ah - BG4SC - BG4 Screen Base and Screen Size (W)
 func (ppu *PPU) bg4sc(data uint8) {
-	ppu.backgroundData.bg[3].screenSize = data & 3
-	ppu.backgroundData.bg[3].tileMapBaseAddress = bit.JoinUint16(0x00, data&^uint8(3))
+	ppu.backgroundData.bg[3].bgsc(data)
 }
 
 // 210Bh/210Ch - BG12NBA/BG34NBA - BG Character Data Area Designation (W)
 func (ppu *PPU) bg12nba(data uint8) {
 	// TODO: use util there
-	ppu.backgroundData.bg[0].tileSetBaseAddress = uint16(data&0x0F) << 12
-	ppu.backgroundData.bg[1].tileSetBaseAddress = bit.JoinUint16(0x00, data&0xF0)
+	ppu.backgroundData.bg[0].tileSetBaseAddr = data & 0x0F
+	ppu.backgroundData.bg[1].tileSetBaseAddr = data >> 4
 }
 
 func (ppu *PPU) bg34nba(data uint8) {
 	// TODO: use util there
-	ppu.backgroundData.bg[2].tileSetBaseAddress = uint16(data&0x0F) << 12
-	ppu.backgroundData.bg[3].tileSetBaseAddress = bit.JoinUint16(0x00, data&0xF0)
+	ppu.backgroundData.bg[2].tileSetBaseAddr = data & 0x0F
+	ppu.backgroundData.bg[3].tileSetBaseAddr = data >> 4
 }
 
 // 210Dh - BG1HOFS - BG1 Horizontal Scroll (X) (W)
@@ -139,4 +146,41 @@ func (ppu *PPU) bg4hofs(data uint8) {
 func (ppu *PPU) bg4vofs(data uint8) {
 	ppu.backgroundData.bg[3].horizontalScroll = bit.JoinUint16(0x00, data) | uint16(ppu.backgroundData.scrollPrev1)
 	ppu.backgroundData.scrollPrev1 = data
+}
+
+// tileAddress returns the byte address in the VRAM where the tile we are looking for is stored
+// See here: https://wiki.superfamicom.org/backgrounds
+func (bg *bg) tileAddress(x uint16, y uint16) uint16 {
+	// TODO: verify that, not sure at all about this
+	var mapIndex uint16
+	if bg.screenSize&0x1 != 0 {
+		mapIndex += x / 32
+	}
+	if bg.screenSize&0x2 != 0 {
+		mapIndex += y / 32
+	}
+
+	base := uint16(bg.tileMapBaseAddr)
+
+	return (base+mapIndex)<<11 + ((y % 32) << 6) + ((x % 32) << 1)
+}
+
+func (ppu *PPU) tileFromBackground(background uint8, x uint16, y uint16) tile {
+	addr := ppu.backgroundData.bg[background].tileAddress(x, y)
+	// raw contains:
+	// vhopppcc cccccccc
+	// v/h        = Vertical/Horizontal flip this tile.
+	// 	o          = Tile priority.
+	// 	ppp        = Tile palette. The number of entries in the palette depends on the Mode and the BG.
+	// 	cccccccccc = Tile number.
+	// See: https://wiki.superfamicom.org/backgrounds
+	raw := bit.JoinUint16(ppu.vram.bytes[addr], ppu.vram.bytes[addr+1])
+
+	return tile{
+		vFlip:    raw&0x8000 != 0,
+		hFlip:    raw&0x4000 != 0,
+		priority: raw&0x2000 != 0,
+		palette:  uint8((raw >> 10) & 0x7),
+		number:   raw & 0x3ff,
+	}
 }
