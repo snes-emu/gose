92717a8d0e81d2e5be430c52e48f5ab553468121
Rémi Calixte <remicalixte.rmc@gmail.com>
2019-12-30 13:48:20 +0100

    fix 212D register

--- core/ppu_display.go
+++ core/ppu_display.go
@@ -45,7 +45,6 @@ func (ppu *PPU) tm(data uint8) {
 func (ppu *PPU) ts(data uint8) {
 	for i := uint8(0); i < 4; i++ {
 		ppu.backgroundData.bg[i].subScreen = data&(1<<i) != 0
-		data = data >> 1
 	}
 	ppu.oam.subScreen = data&0x10 != 0
 }
