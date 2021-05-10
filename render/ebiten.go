d9de2053d103360840aff5cc2f5e631017e6d2f1
Rémi Calixte <remicalixte.rmc@gmail.com>
2021-05-10 19:22:26 +0200

    update ebiten to v2.1.0

--- render/ebiten.go
+++ render/ebiten.go
@@ -11,15 +11,13 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/gobuffalo/packr/v2"
-	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/v2"
 )
 
 //EbitenRenderer is a Renderer implementation using ebiten
 type EbitenRenderer struct {
 	width           int
 	height          int
-	scale           float64
-	title           string
 	offscreenBuffer *ebiten.Image
 	drawOptions     *ebiten.DrawImageOptions
 	running         bool
@@ -29,18 +27,17 @@ type EbitenRenderer struct {
 func newEbitenRenderer(width, height int) (Renderer, error) {
 	//We use this offscreen buffer because we don't want our SNES main loop to be tied to the ebiten one
 	//NewImage always returns a nil error
-	offscreenBuffer, _ := ebiten.NewImage(width, height, ebiten.FilterDefault)
+	offscreenBuffer := ebiten.NewImage(width, height)
 	er := &EbitenRenderer{
 		width:           width,
 		height:          height,
-		scale:           2.0,
-		title:           "Gose",
 		offscreenBuffer: offscreenBuffer,
 		drawOptions:     &ebiten.DrawImageOptions{},
 	}
 
 	ebiten.SetWindowIcon(getWindowLogos())
-	ebiten.SetRunnableInBackground(true)
+	ebiten.SetWindowTitle("Gose")
+	ebiten.SetRunnableOnUnfocused(true)
 
 	return er, nil
 }
@@ -57,28 +54,27 @@ func (er *EbitenRenderer) Render(screen *Screen) {
 
 //SetRomTitle stores the new title if ebiten is not yet running or set the title directly
 func (er *EbitenRenderer) SetRomTitle(title string) {
-	title = fmt.Sprintf("Gose - %s", title)
-	if er.running {
-		ebiten.SetWindowTitle(title)
-	} else {
-		er.title = title
-	}
+	ebiten.SetWindowTitle(fmt.Sprintf("Gose - %s", title))
+
+}
+
+func (er *EbitenRenderer) Update() error {
+	return nil
 }
 
 //update copies the content of the offscreenBuffer to the screen
-func (er *EbitenRenderer) update(screen *ebiten.Image) error {
-	//We should not render if this is true, typically when the app is fully hidden
-	//https://godoc.org/github.com/hajimehoshi/ebiten#IsDrawingSkipped
-	if ebiten.IsDrawingSkipped() {
-		return nil
-	}
-	return screen.DrawImage(er.offscreenBuffer, er.drawOptions)
+func (er *EbitenRenderer) Draw(screen *ebiten.Image) {
+	screen.DrawImage(er.offscreenBuffer, er.drawOptions)
+}
+
+func (er *EbitenRenderer) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return er.width, er.height
 }
 
 //Run starts the ebiten main loop
 //should be called on the main thread
 func (er *EbitenRenderer) Run() {
-	err := ebiten.Run(er.update, er.width, er.height, er.scale, er.title)
+	err := ebiten.RunGame(er)
 	if err != nil {
 		log.Fatal("ebiten crashed", zap.Error(err))
 	}
