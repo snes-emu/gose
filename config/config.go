e8b5b377e8f8535c2699ca4cf4cdd2eaef8e61cb
Sami Tabet <salph.tabet@gmail.com>
2021-05-10 19:54:26 +0200

    render: add a command to render the screen as an image to use test roms more easiliy

--- config/config.go
+++ config/config.go
@@ -6,12 +6,16 @@ var (
 	debugServer bool
 	debugLogs   bool
 	debugPort   int
+	nCycles     int
+	imgPath     string
 )
 
 func init() {
 	flag.BoolVar(&debugServer, "debug-server", false, "enable the debug server")
 	flag.BoolVar(&debugLogs, "debug-logs", false, "enable debug logs")
 	flag.IntVar(&debugPort, "debug-port", 6060, "port the debugger listens to")
+	flag.IntVar(&nCycles, "n-cycles", 10_000_000, "number of CPU cycles to execute before exiting (only used in 'save screen as image mode')")
+	flag.StringVar(&imgPath, "image-path", "", "where to store the screen as an image (the emulator will exit after ${n-cycles} CPU cycles)")
 }
 
 // Inits the config
@@ -32,4 +36,14 @@ func DebugServer() bool {
 // DebugPort is the port the debugger listens to
 func DebugPort() int {
 	return debugPort
+}
+
+// ImagePath is the path to the file we should use to store the screen as an image (it can be empty -> non set)
+func ImagePath() string {
+	return imgPath
+}
+
+// NCycles is the number of CPU cycles we should execute before storing the screen as an image (only used when ImagePath is not empty)
+func NCycles() int {
+	return nCycles
 }
